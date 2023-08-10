c18c94e0ca2d73fb276edbfb33fb32553d43c923
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-10 14:46:30 +0900

    config : aliasの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@/', replacement: `${__dirname}/src/` },
+      { find: '~/', replacement: `${__dirname}/public/`},
+    ],
+  },
   plugins: [react()],
 })
