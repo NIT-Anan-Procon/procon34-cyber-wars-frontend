e3c9a3d688f11c32035ee4e31ab3a0bce52e39a7
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-11 22:49:16 +0900

    style : 使用するのカラー設定

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const colors= {
+  bgDarker: '#1E1E1E',
+  bgLighter: '#D9D9D9',
+  primary: '#2DE7D0',
+  greyAccent: '#8E8E8E',
+  redAccent: '#A72B2B',
+  blueAccent: '#3E63AB',
+  yellowAccent: '#BD9A1C',
+
+  info: '#04e4f4',
+  success: '#20e253',
+  warning: '#f6f000',
+  error: '#fca016',
+  danger: '#f80363',
+  neutral: '#272f4d',
+};
