d83ac5ef98ae31dd4b1784f63903392d019d95cb
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-11 22:50:13 +0900

    style : 入力フォームで使用するのスタイルの設定

--- src/styles/dimensions.ts
+++ src/styles/dimensions.ts
@@ -1,15 +1,24 @@
 export type InputSizeProps= 'small' | 'medium' | 'large';
 
 export const InputSize= (inputSize?: InputSizeProps) => {
-  const sizeSpecificStyles= {
+  const sizeSpecificStyles = {
     small: `
-    
+      font-size: 1rem;
+      border-radius: 0.25rem;
+      padding: 0.5rem 1rem;
+      margin: 0.5rem;
     `,
     medium: `
-    
+      font-size: 1.5rem;
+      border-radius: 0.25rem;
+      padding: 0.75rem 1.5rem;
+      margin: 0.5rem;
     `,
     large: `
-    
+      font-size: 2rem;
+      border-radius: 0.25rem;
+      padding: 1rem 1.75rem;
+      margin: 0.5rem;
     `,
   };
   switch (inputSize) {
