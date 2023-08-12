333b5317a867b3a980f9b27bf23bc1069a43b4af
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-12 20:52:46 +0900

    feat : リロードとブラウザバック禁止

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+
 import { AppProvider } from '@/provider/app';
 import { AppRoutes } from './routes';
 
 function App() {
+    useEffect(() => {
+    const onUnload = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+      e.returnValue = "";
+    };
+    window.addEventListener("beforeunload", onUnload);
+    window.history.pushState(null, '', window.location.href);
+    window.addEventListener("popstate", () => {
+      history.go(1);
+    });
+  });
   return (
     <AppProvider>
       <AppRoutes />
