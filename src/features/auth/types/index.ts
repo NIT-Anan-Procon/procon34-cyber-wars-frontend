2ed1de5b5a6ea83a4936a8995ce4efd229a3e9ec
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-13 00:06:59 +0900

    add : フォームにバリデーションを当てる際のスキーマを設定

--- src/features/auth/types/index.ts
+++ src/features/auth/types/index.ts
@@ -1,11 +1,18 @@
 import { z } from 'zod';
 
-export type AuthUser= {
-  userName: string;
-  password: string;
-}
+export type AuthUser = {
+	userName: string;
+	password: string;
+};
 
-export const FormSchema= {
-  userName: z.string().min(1).max(20),
-  password: z.string().min(4).max(100),
-}
+export const FormSchema= z.object({
+  userName: z
+		.string()
+		.nonempty('ユーザ名は必須です。')
+		.max(20, '20文字以内で入力してください。'),
+	password: z
+		.string()
+		.nonempty('パスワードは必須です。')
+		.min(4, '4文字以上で入力してください。')
+		.max(100, '100文字以内で入力してください。'),
+})
