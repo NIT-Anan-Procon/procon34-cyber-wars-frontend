f6449579c31eaab0aac9bda325870f23d2c98148
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-13 00:05:50 +0900

    add : initial commit

--- src/components/Elements/Button.tsx
+++ src/components/Elements/Button.tsx
@@ -1,5 +1,44 @@
-export const Button= () => {
-  return(
-    <button
-  );
-};
+import { forwardRef } from 'react';
+import styled         from 'styled-components';
+
+import { Loading } from '.';
+
+const ButtonStyle= styled.button`
+  width: 100%;
+`;
+
+type IconProps =
+  | { startIcon: React.ReactElement; endIcon?: never }
+  | { endIcon: React.ReactElement; startIcon?: never }
+  | { endIcon?: undefined; startIcon?: undefined };
+
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  isLoading?: boolean;
+} & IconProps;
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      type = 'button',
+      isLoading = false,
+      startIcon,
+      endIcon,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <ButtonStyle
+        ref={ref}
+        type={type}
+        {...props}
+      >
+        {isLoading && <Loading />}
+        {!isLoading && startIcon}
+        <span>{props.children}</span> {!isLoading && endIcon}
+      </ButtonStyle>
+    );
+  }
+);
+
+Button.displayName = 'Button';
--- src/features/auth/components/SignUpForm.tsx
+++ src/features/auth/components/SignUpForm.tsx
@@ -1,9 +1,71 @@
-type SignUpFormProps= {
-  onSuccess: () => void;
-}
+import styled          from "styled-components";
+import { Link }        from "react-router-dom";
+import { useForm }     from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export const SignUpForm= ({ onSuccess }: SignUpFormProps) => {
-  return(
-    <>SignUp</>
+import { useAuth }               from "@/libs/auth";
+import { FormTitle, InputField } from "@/components/Form";
+import { Button }                from '@/components/Elements/Button';
+import { AuthUser, FormSchema }  from "..";
+
+
+type SignUpFormProps = {
+	onSuccess: () => void;
+};
+
+const FormStyle= styled.form`
+  width         : clamp(40rem, 40vw, 60rem);
+  height        : 80%;
+  display       : flex;
+  flex-direction: column;
+  row-gap       : 1rem;
+  background    : white;
+`;
+
+export const SignUpForm = ({onSuccess}: SignUpFormProps) => {
+	const { isSignIn }= useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AuthUser>({
+    mode: "onChange",
+    shouldUnregister: false,
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = async(data: AuthUser) => {
+    console.log("Submitted Data", data);
+		await isSignIn(data);
+		onSuccess();
+  };
+
+  return (
+    <FormStyle onSubmit={handleSubmit(onSubmit)} >
+      <FormTitle title={ 'Sign Up' } />
+      <>
+        <InputField
+          id   = { 'userName' }        
+          type = { 'text' }
+          size = { 'medium' }
+          label= { 'ユーザ名' }
+          error= { errors.userName }
+          registration= { register('userName') }      
+        />
+        <InputField
+          id   = { 'password' }
+          type = { 'password' }
+          size = { 'medium' }
+          label= { 'パスワード' }
+          error= { errors.password }
+          registration= { register('password') }
+        />      
+      </>
+      <Button type="submit">送信</Button>
+      <p>
+        Already have an account?
+        <Link to='../sign-in'>Sign in</Link>
+      </p>
+    </FormStyle>
   );
-}
+};
