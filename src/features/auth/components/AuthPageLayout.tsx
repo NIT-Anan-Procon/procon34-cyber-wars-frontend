183bb94a6d433d2e74fd07256275d1c259b1c4c9
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-13 00:11:02 +0900

    add : ロゴ用のスペースをレイアウトに追加

--- src/features/auth/components/AuthPageLayout.tsx
+++ src/features/auth/components/AuthPageLayout.tsx
@@ -1,36 +1,46 @@
 import styled from 'styled-components';
 
 import { colors } from '@/styles/colors';
+import { Head } from '@/components/Head';
 
 type AuthPageLayoutProps= {
+  title   : string,
   children: React.ReactNode;
 }
 
 const GridLayout= styled.div`
-  height: 100%;
-  width: 100%;
+  height : 100%;
+  width  : 100%;
   display: grid;
   grid-template-columns: 50% 50%;
-  column-gap: 5rem;
+  grid-template-rows   : 15% 1fr;
 `;
 
 const ImageContents= styled.div`
-  grid-column: 1;
-  margin: 5rem;
-  height: calc(100% - 10rem);
-  width : 100%;
-  background: ${ colors.bgLighter };
+  grid-column  : 1;
+  grid-row     : 1 / span 2;
+  margin       : 5rem;
+  height       : calc(100% - 10rem);
+  width        : 100%;
+  background   : ${ colors.bgLighter };
   border-radius: 1rem;
-  box-shadow: 0px 0px 10px 3px #3a3a3a inset;
+  box-shadow   : 0px 0px 10px 3px #3a3a3a inset;
 `;
 
 const ContentsWrapper= styled.div`
-  width: 100%;
+  width  : 100%;
   display: flex;
   justify-content: center;
 `;
 
-const FormWrapper= styled.div`
+const LogoArea= styled.div`
+  grid-column: 2;
+  grid-row   : 1 / 2;
+  width      : 100%;
+  height     : 100%;
+  background : grey;
+`;
+const FormContainer= styled.div`
   grid-column: 2;
   height: 100%;
   width: 100%;
@@ -39,17 +49,23 @@ const FormWrapper= styled.div`
   align-items: center;
 `;
 
-export const AuthPageLayout= ({ children }: AuthPageLayoutProps) => {
+export const AuthPageLayout= ({ title, children }: AuthPageLayoutProps) => {
   return (
-    <GridLayout>
-      <ImageContents>
-        <ContentsWrapper>
-          注意書きと画像
-        </ContentsWrapper>
-      </ImageContents>
-      <FormWrapper>
-        {children}
-      </FormWrapper>
-    </GridLayout>
+    <>
+      <Head title= {title} />
+      <GridLayout>
+        <ImageContents>
+          <ContentsWrapper>
+            注意書きと画像
+          </ContentsWrapper>
+        </ImageContents>
+        <LogoArea>
+          logo
+        </LogoArea>
+        <FormContainer>
+          {children}
+        </FormContainer>
+      </GridLayout>    
+    </>
   );
 }
