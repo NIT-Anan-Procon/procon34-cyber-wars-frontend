622580d1cde481b32bc4100d35ce2225714fbb52
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-13 00:02:35 +0900

    add : 各種プロバイダでラップ

--- src/provider/app.tsx
+++ src/provider/app.tsx
@@ -1,17 +1,23 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter as Router } from 'react-router-dom';
 import { RecoilRoot } from "recoil";
 import { Suspense } from 'react';
+import { AuthProvider } from './authProvider';
+import { HelmetProvider } from 'react-helmet-async';
 
 type AppProviderProps = {
   children: React.ReactNode;
 }
 
 export const AppProvider= ({children}: AppProviderProps) => {
-  return(
-    <Suspense fallback={<div>loading</div>}>
+  return(      
+    <HelmetProvider>
       <RecoilRoot>
-        <BrowserRouter>{children}</BrowserRouter>      
-      </RecoilRoot>      
-    </Suspense>
+        <Suspense fallback={<div>loading</div>}>
+          <AuthProvider>
+            <Router>{children}</Router>            
+          </AuthProvider>
+        </Suspense>        
+      </RecoilRoot>         
+    </HelmetProvider>
   );
 };
