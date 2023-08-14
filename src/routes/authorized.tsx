f32de6aae5b11b45c8a4b66af7d0ddf7c1fed6f8
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-15 00:30:12 +0900

    add : Not Foundのページ追加

--- src/routes/authorized.tsx
+++ src/routes/authorized.tsx
@@ -1,10 +1,16 @@
-import { ModeSelection } from '@/features/views';
 import { Suspense } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+
+import { lazyImport } from '@/utils/lazyImport';
+import { NotFound } from '@/features/views';
+
+const { ModeSelection } = lazyImport(() => import('@/features/views'), 'ModeSelection');
 
 const App = () => {
   return (
-    <>d</>
+    <Suspense fallback={<div>Loading</div>}>
+      <Outlet />
+    </Suspense>
   );
 };
 
@@ -13,14 +19,14 @@ export const authorizedRoutes = [
     path: '/cyberwars',
     element: <App />,
     children: [
-      { path: '/',                element: <ModeSelection /> },
-      // { path: '/training-mode',   element: <TrainingMode /> },
+      { path: '',                element: <ModeSelection /> },
+      // { path: '/training-mode/*',   element: <TrainingMode /> },
       // { path: '/stand-by',        element: <StandBy /> },
       // { path: '/attack-phase',    element: <AttackPhase /> },
       // { path: '/defence-phase',   element: <DefencePhase /> },
       // { path: '/battle-phase',    element: <BattlePhase /> },
       // { path: '/commentary-mode', element: <CommentaryMode /> },
-      // { path: '*',                element: <Navigate to='.' /> },
+      { path: '*',                element: <NotFound /> },
     ],
   },
 ];
