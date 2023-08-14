de63145eb4dd4ae4fa70cf4d2b185b1455763bdb
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-15 00:29:04 +0900

    refactor : atom名変更

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,20 @@
 import { useRoutes } from 'react-router-dom';
 
 import { Landing } from '@/features/views';
-// import { useAuth } from '@/libs/auth';
 
 import { authorizedRoutes } from './authorized';
 import { publicRoutes } from './public';
 import { useRecoilValue } from 'recoil';
-import { authState } from '@/atoms';
+import { isAuthState } from '@/atoms';
 
 export const AppRoutes = () => {
-  const isAuthenticated= useRecoilValue<boolean>(authState);
+  const isAuthenticated= useRecoilValue<boolean>(isAuthState);
 
   const commonRoutes= [{ path: '/', element: <Landing /> }];
-
-  const routes = isAuthenticated ? authorizedRoutes : publicRoutes;
-
-  const element = useRoutes([...routes, ...commonRoutes]);
   
+  const routes = isAuthenticated ? authorizedRoutes : publicRoutes;
+  
+  const element = useRoutes([...routes, ...commonRoutes]);
+
   return <>{element}</>;
 }
