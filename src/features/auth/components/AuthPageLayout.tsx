f73ff7a754ef6a2b590d7d6d205117da52994f43
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-14 23:22:43 +0900

    style : gridのレイアウト修正

--- src/features/auth/components/AuthPageLayout.tsx
+++ src/features/auth/components/AuthPageLayout.tsx
@@ -13,40 +13,43 @@ const GridLayout= styled.div`
   width  : 100%;
   display: grid;
   grid-template-columns: 50% 50%;
-  grid-template-rows   : 15% 1fr;
 `;
 
 const ImageContents= styled.div`
-  grid-column  : 1;
-  grid-row     : 1 / span 2;
-  margin       : 5rem;
-  height       : calc(100% - 10rem);
-  width        : 100%;
-  background   : ${ colors.bgLighter };
-  border-radius: 1rem;
-  box-shadow   : 0px 0px 10px 3px #3a3a3a inset;
+  grid-column    : 1;
+  padding        : 5rem 0rem 5rem 5rem;
+  height         : 100%;
+  width          : 100%;  
 `;
 
 const ContentsWrapper= styled.div`
-  width  : 100%;
-  display: flex;
+  width          : 100%;
+  height         : 100%;
+  display        : flex;
   justify-content: center;
+  background     : ${ colors.bgLighter };
+  border-radius  : 1rem;
+  box-shadow     : 0px 0px 10px 3px #3a3a3a inset;
 `;
 
-const LogoArea= styled.div`
-  grid-column: 2;
-  grid-row   : 1 / 2;
-  width      : 100%;
-  height     : 100%;
-  background : grey;
-`;
 const FormContainer= styled.div`
-  grid-column: 2;
-  height: 100%;
-  width: 100%;
-  display: flex;
+  grid-column   : 2;
+  padding       : 5rem 0;
+  height        : 100%;
+  width         : 100%;
+  display       : flex;
+  flex-direction: column;
+  align-items   : center;
+  row-gap       : 2rem;
+`;
+
+const LogoArea= styled.div` 
+  width          : 50%;
+  height         : 10%;
+  display        : flex;
   justify-content: center;
-  align-items: center;
+  align-items    : center;
+  background     : grey;
 `;
 
 export const AuthPageLayout= ({ title, children }: AuthPageLayoutProps) => {
@@ -59,10 +62,10 @@ export const AuthPageLayout= ({ title, children }: AuthPageLayoutProps) => {
             注意書きと画像
           </ContentsWrapper>
         </ImageContents>
-        <LogoArea>
-          logo
-        </LogoArea>
         <FormContainer>
+          <LogoArea>
+            logo
+          </LogoArea>
           {children}
         </FormContainer>
       </GridLayout>    
