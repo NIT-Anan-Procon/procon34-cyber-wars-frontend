bead132a3464019ee44857d21cedf713f3522f2e
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-17 04:07:01 +0900

    fix : path変更

--- src/features/mode-select/routes/index.tsx
+++ src/features/mode-select/routes/index.tsx
@@ -1,14 +1,14 @@
 import { Landing, NotFound } from "@/features/views";
 import { Route, Routes } from "react-router-dom";
-import { ModeSelection } from "./modeSelection";
+import { ModeSelection } from "./ModeSelection";
 import { MATCH_MODE_PATH, TRAIN_MODE_PATH } from "../types";
 
 export const ModeSelectionRoutes= () => {
   return (
     <Routes>
       <Route path='/' element={<ModeSelection />} />
-      <Route path={`${TRAIN_MODE_PATH}`} element={<Landing/>} />
-      <Route path={`${MATCH_MODE_PATH}`} element={<NotFound/>} />
+      <Route path={`${TRAIN_MODE_PATH}/*`} element={<Landing/>} />
+      <Route path={`${MATCH_MODE_PATH}/*`} element={<NotFound/>} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   );
