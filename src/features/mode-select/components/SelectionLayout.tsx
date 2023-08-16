258084edc964aae497eeb91f0349941edaeee122
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-17 06:02:11 +0900

    style : gapの修正

--- src/features/mode-select/components/SelectionLayout.tsx
+++ src/features/mode-select/components/SelectionLayout.tsx
@@ -6,23 +6,22 @@ const FlexColumnLayout= styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  column-gap: 10%;
 `;
 
 const ContentWrapper= styled.div`
   width : 100%;
   height: 100%;
-  padding: 3rem 20% 3rem 20%;
+  padding: 5rem 20% 3rem 20%;
   display: flex;
   flex-direction: column;
-  row-gap: 5rem;
+  row-gap: 10rem;
 `;
 
 type LayoutProps= {
   children: React.ReactNode;
 };
 
-export const Layout= ({ children }: LayoutProps) => {
+export const SelectionLayout= ({ children }: LayoutProps) => {
   return (
     <FlexColumnLayout>
       <ContentWrapper>{children}</ContentWrapper>
