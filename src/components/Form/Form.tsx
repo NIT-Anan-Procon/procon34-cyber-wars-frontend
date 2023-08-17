a1b305ee696b34315ea27948f7bf932bbc927930
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-17 21:10:39 +0900

    refactor : 可読性向上のため、typeとstyleの位置変更

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,14 +3,6 @@ import { ZodType, ZodTypeDef } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, UseFormReturn, UseFormProps, SubmitHandler } from 'react-hook-form';
 
-type FormProps<TFormValues extends Record<string, any>, Schema extends ZodType<any, ZodTypeDef, any>> = {
-  onSubmit: SubmitHandler<TFormValues>;
-  children: (methods: UseFormReturn<TFormValues>) => React.ReactNode;
-  options?: UseFormProps<TFormValues>;
-  id?: string;
-  schema?: Schema;
-};
-
 const FormContainer = styled.form`
   width         : clamp(40rem, 40vw, 100%);
   height        : calc(85% - 2rem);
@@ -24,6 +16,14 @@ const FormContainer = styled.form`
   border-radius : 2.75rem;
   box-shadow    : 0px 187px 75px rgba(0, 0, 0, 0.01), 0px 105px 63px rgba(0, 0, 0, 0.05), 0px 47px 47px rgba(0, 0, 0, 0.09), 0px 12px 26px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1);
 `;
+
+type FormProps<TFormValues extends Record<string, any>, Schema extends ZodType<any, ZodTypeDef, any>> = {
+  onSubmit: SubmitHandler<TFormValues>;
+  children: (methods: UseFormReturn<TFormValues>) => React.ReactNode;
+  options?: UseFormProps<TFormValues>;
+  id?: string;
+  schema?: Schema;
+};
 
 export const Form = <
   TFormValues extends Record<string, unknown> = Record<string, unknown>,
