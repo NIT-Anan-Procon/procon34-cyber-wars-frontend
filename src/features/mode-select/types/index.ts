6bf134f6bf245374563f086524a655cc1482d4c9
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-18 01:22:12 +0900

    add : 各モードの説明文を追加

--- src/features/mode-select/types/index.ts
+++ src/features/mode-select/types/index.ts
@@ -9,3 +9,11 @@ export const ROOM_MODES= {
 
 export const TRAIN_MODE_PATH= '/train';
 export const MATCH_MODE_PATH= '/match';
+
+export const CARD_DESCRIPTION= {
+  TRAIN_DESCRIPTION: 
+    `訓練モードでは、ゲームの進め方、操作方法、ゲームルールを解説します。簡単な攻撃方法と脆弱性の修正方法を学び、対戦に向けて訓練できます。`
+  ,
+  MATCH_DESCRIPTION: 
+    `対戦モードでは、「アタックフェーズ」、「ディフェンスフェーズ」、「バトルフェーズ」の３つのフェーズを通して対戦し、合計のポイントを競います。`
+}
