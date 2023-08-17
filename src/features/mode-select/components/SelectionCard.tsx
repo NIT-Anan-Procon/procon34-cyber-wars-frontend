a5026b69950a70da13989df528233ac32a7a7cf1
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-18 01:26:34 +0900

    style : UIの大幅変更

--- src/features/mode-select/components/SelectionCard.tsx
+++ src/features/mode-select/components/SelectionCard.tsx
@@ -1,55 +1,82 @@
 import { colors } from '@/styles';
 import styled from 'styled-components';
 
-
-const GroupLayout= styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+const StyledCard= styled.div`
+  width: clamp(50rem, 50vw, 60rem);
+  height: clamp(50rem, 60vh, 65rem);
+  padding: 5rem;
+  background: grey;
+  box-sizing: border-box;
+  background: ${colors.bgLighter};
+  border: 3px solid white;
+  box-shadow: 12px 17px 51px rgba(0, 0, 0, 0.22);
+  backdrop-filter: blur(6px);
+  border-radius: 17px;
+  cursor: pointer;
+  transition: all 0.5s;
 `;
 
-const ModeContentWrapper= styled.div`
+const CardContainer= styled.div`
   width: 100%;
   height: 100%;
+  display: grid;
+  grid-template-rows: 15% 1fr 30%;
 `;
 
-const ModeTitle= styled.h1`
+const CardTitle= styled.h1`
+  grid-row: 1;
+  font-size: 3rem;
+  font-weight: 600;
+  text-align : center;
+`;
+
+const ImageContainer= styled.div`
+  grid-row: 2;
   width: 100%;
-  line-height: 4rem;
-  font-size: 2rem;
-  color: ${colors.secondary};
-  border-bottom: 3px solid ${colors.secondary};
-`;
-
-const InputsLayout= styled.div`
-  position: relative;
-  width  : 100%;
-  height : 100%;
+  height: 10rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  column-gap: 20%;
+`;
+
+const CardDescription= styled.p`
+  grid-row: 3;
+  font-size  : 1.5rem;
+  font-weight: 500;
+  color      : #808080;
 `;
 
 
-type SelectionGroupProps= {
+type SelectionCardProps= {
+  id: string;
   title: string;
-  description: string;
-  children: React.ReactNode;
+  imgPath?: string | undefined;
+  description: string;  
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  active?: boolean;
 }
 
-
-export const SelectionCard= ({title, description, children}: SelectionGroupProps) => {
+export const SelectionCard= (
+  { 
+    id,
+    title, 
+    imgPath, 
+    description,
+    onClick, 
+    active,
+    ...props 
+  }: SelectionCardProps
+) => {
   return (
-    <GroupLayout>
-      <ModeContentWrapper>
-        <ModeTitle>{title}</ModeTitle>
-        <InputsLayout>
-          {children}
-        </InputsLayout>
-      </ModeContentWrapper>
-    </GroupLayout>
+    <StyledCard id={id} onClick={onClick} {...props} >
+      <CardContainer>
+        <CardTitle>{title}</CardTitle>
+        <ImageContainer>
+          image
+          <img src={imgPath} />
+        </ImageContainer>
+        <CardDescription>{description}</CardDescription>
+      </CardContainer>
+    </StyledCard>
   );
 }
