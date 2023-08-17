94d092f9022a68f2214c7c2a84eb54688e1eea39
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-18 02:41:40 +0900

    add : 対戦モードのルーティング設定追加

--- src/routes/authorized.tsx
+++ src/routes/authorized.tsx
@@ -3,12 +3,13 @@ import { Outlet } from 'react-router-dom';
 
 import { lazyImport } from '@/utils/lazyImport';
 import { NotFound } from '@/features/views';
+import { MatchModeRoutes } from '@/features/mode';
 
 const { ModeSelectionRoutes } = lazyImport(() => import('@/features/mode-select'), 'ModeSelectionRoutes');
 
 const App = () => {
   return (
-    <Suspense fallback={<div>Loading</div>}>
+    <Suspense fallback={<div>Loading</div>} >
       <Outlet />
     </Suspense>
   );
@@ -19,14 +20,9 @@ export const authorizedRoutes = [
     path: '/cyberwars',
     element: <App />,
     children: [
-      { path: '', element: <ModeSelectionRoutes /> },
-      // { path: '/training-mode/*',   element: <TrainingMode /> },
-      // { path: '/stand-by',        element: <StandBy /> },
-      // { path: '/attack-phase',    element: <AttackPhase /> },
-      // { path: '/defence-phase',   element: <DefencePhase /> },
-      // { path: '/battle-phase',    element: <BattlePhase /> },
-      // { path: '/commentary-mode', element: <CommentaryMode /> },
-      { path: '*',                element: <NotFound /> },
+      { path: '',  element: <ModeSelectionRoutes /> },
+      { path: 'match/*', element: <MatchModeRoutes /> },
+      { path: '*', element: <NotFound /> },
     ],
   },
 ];
