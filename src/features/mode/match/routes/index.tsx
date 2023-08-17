bfe8b09be3d20192df9bd420c6ec9cb5993f33cc
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-18 02:26:40 +0900

    add : 呼び出すコンポーネントの追加

--- src/features/mode/match/routes/index.tsx
+++ src/features/mode/match/routes/index.tsx
@@ -1,6 +1,14 @@
-import { NotFound } from "@/features/views";
 import { Route, Routes } from "react-router-dom";
+
+import { NotFound } from "@/features/views";
 import { PHASE_PATH } from "../types";
+import { 
+  AttackPhase, 
+  BattlePhase, 
+  DefencePhase, 
+  ExplanationPhase, 
+  StandBy 
+} from "../components";
 
 export const MatchModeRoutes= () => {
   return (
