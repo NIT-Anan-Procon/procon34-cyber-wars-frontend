e48da5d45c8a119933d4a48abca57c72dbefd629
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-18 02:55:19 +0900

    add : 対戦モードのルーティングを遅延読み込み

--- src/routes/authorized.tsx
+++ src/routes/authorized.tsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom';
 
 import { lazyImport } from '@/utils/lazyImport';
 import { NotFound } from '@/features/views';
-import { MatchModeRoutes } from '@/features/mode';
 
-const { ModeSelectionRoutes } = lazyImport(() => import('@/features/mode-select'), 'ModeSelectionRoutes');
+const { ModeSelection }= lazyImport(() => import('@/features/mode-select'), 'ModeSelection');
+const { MatchModeRoutes }= lazyImport(() => import('@/features/mode'), 'MatchModeRoutes');
 
 const App = () => {
   return (
@@ -17,10 +17,10 @@ const App = () => {
 
 export const authorizedRoutes = [
   {
-    path: '/cyberwars',
+    path: 'cyberwars',
     element: <App />,
     children: [
-      { path: '',  element: <ModeSelectionRoutes /> },
+      { path: '',  element: <ModeSelection /> },
       { path: 'match/*', element: <MatchModeRoutes /> },
       { path: '*', element: <NotFound /> },
     ],
