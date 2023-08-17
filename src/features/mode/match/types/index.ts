9de7e10490df01f259f77fc455cc68b3017160d9
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-18 02:27:45 +0900

    add : フェーズごとのパス設定

--- src/features/mode/match/types/index.ts
+++ src/features/mode/match/types/index.ts
@@ -1,6 +1,1 @@
-export const PHASE_PATH= {
-  ATTACK_PHASE_PATH: '/attack-phase',
-  DEFENCE_PHASE_PATH: '/defence-phase',
-  BATTLE_PHASE_PATH: '/battle-phase',
-  EXPLANATION_PHASE_PATH: '/explanation-phase',
-}
+export * from './phaseRoutes';
