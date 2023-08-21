13588bc3da1ffba27d0a3bd41804c2b9dac240ba
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-21 23:30:40 +0900

    add : initial commit

--- src/features/mode/match/routes/StandBy.tsx
+++ src/features/mode/match/routes/StandBy.tsx
@@ -1,5 +1,23 @@
+import { Head } from "@/components/Head";
+import { RoomSelectForm } from "@/features/rooms";
+import styled from "styled-components";
+
+const StandByLayout= styled.div`
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 40vw 1fr 40vw;
+  
+`;
+
 export const StandBy= () => {
   return (
-    <></>
+    <>
+      <Head title='待機画面' />
+      <RoomSelectForm />
+      <StandByLayout>
+        
+      </StandByLayout>    
+    </>
   );
 };
