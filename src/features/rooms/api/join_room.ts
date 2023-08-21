5215361f3596480ba0748731ee5582a749124ef8
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-21 23:25:51 +0900

    add : ルーム参加api関連の処理をするカスタムフックを作成

--- src/features/rooms/api/join_room.ts
+++ src/features/rooms/api/join_room.ts
@@ -1,3 +1,33 @@
-export const join_room= () => {
+import { ROOMS_URL } from './config/rooms_endpoints';
+import { INVITE_ID_KEY } from './config/rooms_keys';
+import axios from "axios"
+import { RoomFormType } from ".."
+import { useSetRecoilState } from "recoil";
+import { RoomIdState } from "@/atoms";
+
+export const useJoinRoom= () => {
   
+  const setRoomId= useSetRecoilState<number>(RoomIdState);
+
+  async function isJoinedRoom(data: number): Promise<void>{
+    try {
+      const formattedJsonData= JSON.stringify(data);
+      const response= await axios.post(
+        ROOMS_URL, 
+        formattedJsonData,  
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      //setRoomId(response.data.INVITE_ID_KEY);
+      setRoomId(1234);
+    }
+    catch(error) {
+      console.log('error');
+    }
+  }
+
+  return { isJoinedRoom };
 }
