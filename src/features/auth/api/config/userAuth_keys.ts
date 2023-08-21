f85e30182c5022db5e75ccc7dbd8760f599521b8
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-21 23:22:06 +0900

    add : apiのkeyを設定

--- src/features/auth/api/config/userAuth_keys.ts
+++ src/features/auth/api/config/userAuth_keys.ts
@@ -1,3 +1,3 @@
 export const USER_NAME_KEY    = import.meta.env.VITE_API_KEY_USER_NAME;
 export const USER_PASSWORD_KEY= import.meta.env.VITE_API_KEY_USER_PASSWORD;
-export const IS_SUCCESS_KEY    = import.meta.env.VITE_API_KEY_USER_ISAUTH;
+export const ISSUCCESS_KEY    = import.meta.env.VITE_API_KEY_USER_ISAUTH;
