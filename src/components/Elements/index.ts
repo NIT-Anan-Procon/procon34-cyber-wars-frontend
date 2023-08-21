eae1c2f664b7fac132186e29f14cb0407e5f2a2f
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-22 01:22:16 +0900

    add : initial commit

--- src/components/Elements/index.ts
+++ src/components/Elements/index.ts
@@ -2,6 +2,7 @@ export * from './Header';
 export * from './Button';
 export * from './Link';
 export * from './RadioButton';
+export * from './Card';
 export * from './Dialog';
 export * from './Loading';
 export * from './SnackBar';
