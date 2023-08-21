ea03fbccae35eeb9ede7aa5a859a0669a58040cd
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-21 23:20:04 +0900

    add : initial commit

--- src/components/Elements/Dialog.tsx
+++ src/components/Elements/Dialog.tsx
@@ -1,9 +1,29 @@
+import { useRef } from 'react';
+import styled from 'styled-components';
+
+const DialogLayout= styled.dialog`
+
+`;
+
+const DialogContent= styled.div`
+
+`;
+
 type DialogProps= {
+  isOpen: boolean
   children: React.ReactNode;
 }
 
-export const Dialog= ({children}: DialogProps) => {
+export const Dialog= ({
+  isOpen, children
+}: DialogProps): React.ReactElement => {
+  const dialogRef= useRef<HTMLDialogElement>(null);
+  
   return (
-    <>{children}</>
+    <DialogLayout ref={dialogRef} >
+      <DialogContent>
+        {children}
+      </DialogContent>
+    </DialogLayout>
   );
 }
