b917ea966feeec45b8704752aa9b6c275dda3630
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-22 09:45:50 +0900

    add : モーダルを表示させるカスタムフックを作成

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,19 @@
+import { useRef, useCallback } from "react"
+
+export const useModal= () => {
+  const ref: React.MutableRefObject<HTMLDialogElement | null>= useRef(null);
+
+  const showModal= useCallback(() => {
+    if(ref.current) {
+      ref.current.showModal();
+    }
+  },[]);
+  
+  const closeModal= useCallback(() => {
+    if(ref.current) {
+      ref.current.close();
+    }
+  },[]);
+
+  return { ref, showModal, closeModal };
+}
