4fac6ffe113d9cd6e556962c8379c88a81022244
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-29 04:57:01 +0900

    remove : 不要なインポート削除

--- src/features/mode/match/routes/index.tsx
+++ src/features/mode/match/routes/index.tsx
@@ -1,6 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import { NotFound } from "@/features/views";
 import { PHASE_PATH } from "../types";
 import { StandBy } from "@/features/standby/routes/StandBy";
 import { AttackPhase } from './AttackPhase';
