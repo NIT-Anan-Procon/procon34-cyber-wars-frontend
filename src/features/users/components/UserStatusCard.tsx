19d64321ca9568c39b2701efe817fb1067fb9c3d
kusaka0324 <haruto.k.4230@gmail.com>
2023-08-29 13:42:28 +0900

    add : initial commit

---
+++ src/features/users/components/UserStatusCard.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+type UserStatusCardProps= {
+  userName: string,
+  status: string
+}
+
+export const UserStatusCard= ({ userName, status }: UserStatusCardProps) => {
+  return (
+    <></>
+  );
+}
