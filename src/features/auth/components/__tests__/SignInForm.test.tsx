84f771b426547ade7122db9f9fef380bd221e2fb
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-02 03:02:11 +0900

    add : initial commit

---
+++ src/features/auth/components/__tests__/SignInForm.test.tsx
@@ -0,0 +1,25 @@
+import { server } from "@/mocks/server";
+import { SignInForm } from "../SignInForm"
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+beforeAll(() => server.listen());
+  
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+
+afterAll(() => server.close());
+  
+describe('ユーザ認証関連のAPIテスト',() => {
+  test('ログイン成功時のテスト', async() => {
+    const onSuccess = jest.fn();
+
+    render(<SignInForm onSuccess={onSuccess}/>)
+
+  }),
+  test('ログイン失敗のテスト'), async() => {
+
+  }
+});
