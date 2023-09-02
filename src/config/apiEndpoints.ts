4f132d8a0f41f551fc4dde1c429c30a2259784a1
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-03 04:41:42 +0900

    fix : エンドポイント変更

--- src/config/apiEndpoints.ts
+++ src/config/apiEndpoints.ts
@@ -1,25 +1,26 @@
 //ユーザ認証関連のエンドポイント
-export const SIGNUP_USER_URL  = '/users';
-export const USER_NAME_URL    = '/users/name';
-export const USER_PASSWORD_URL= '/users/password';
-export const SIGNIN_USER_URL  = '/users/credentials';
-export const SIGNOUT_USER_URL = '/users/credentials';
+export const SIGNUP_USER_URL  = '/user';
+export const UPDATE_USER_NAME_URL    = '/user/name';
+export const UPDATE_USER_PASSWORD_URL= '/user/password';
+export const SIGNIN_USER_URL = '/user/credential';
+export const IS_LOGGEDIN_URL = '/user/credential'
+export const SIGNOUT_USER_URL= '/user/credential';
 
 //ルーム関連のエンドポイント
-export const CREATE_ROOM_URL  = '/rooms';
-export const JOIN_ROOM_URL    = '/rooms';
-export const GET_ROOM_INFO_URL= '/rooms';
-export const LEAVE_ROOM_URL   = '/rooms';
+export const CREATE_ROOM_URL  = '/room';
+export const JOIN_ROOM_URL    = '/room';
+export const GET_ROOM_INFO_URL= '/room';
+export const LEAVE_ROOM_URL   = '/room';
 
 //ゲーム関連のエンドポイント
-export const IS_START_GAME_URL      = '/games';
-export const GET_GAME_START_TIME_URL= '/games';
-export const GAME_ENDS_URL          = '/games';
-export const GET_PLAYERS_INFO_URL   = '/games/players';
-export const GET_SCORE_INFO_URL     = '/games/scores';
-export const GET_CHALLENGE_WEB_URL  = '/games/challenge';
-export const USED_HINT_URL          = '/games/hint';
-export const SEND_KEY_URL           = '/games/key';
-export const GET_SOURCE_CODE_URL    = '/games/code';
-export const SEND_SOURCE_CODE_URL   = '/games/code';
-export const GET_FIXED_WEB_URL      = '/games/revision'
+export const IS_START_GAME_URL      = '/game';
+export const GET_GAME_START_TIME_URL= '/game/start-time';
+export const GAME_ENDS_URL          = '/game';
+export const GET_PLAYERS_INFO_URL   = '/game/players';
+export const GET_SCORE_INFO_URL     = '/game/scores';
+export const GET_CHALLENGE_WEB_URL  = '/game/challenge';
+export const USED_HINT_URL          = '/game/hint';
+export const SEND_KEY_URL           = '/game/key';
+export const GET_SOURCE_CODE_URL    = '/game/code';
+export const SEND_SOURCE_CODE_URL   = '/game/code';
+export const GET_FIXED_WEB_URL      = '/game/revision'
