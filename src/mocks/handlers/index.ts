81784e66e0e1fc18623b8263b915777c85fa0468
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-03 04:05:17 +0900

    add : ゲーム関連のapiのハンドラーを追加

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,8 +1,9 @@
+import { gameHandlers } from "./game";
 import { roomHandlers } from "./room";
 import { userHandlers } from "./user";
 
 export const handlers= [
   ...userHandlers,
   ...roomHandlers,
-  
+  ...gameHandlers,
 ];
