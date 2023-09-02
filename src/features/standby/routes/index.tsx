52a8e7f5247978cb623c1dcff28820914921a92e
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-03 04:14:15 +0900

    add : 待機画面のルーティング設定

--- src/features/standby/routes/index.tsx
+++ src/features/standby/routes/index.tsx
@@ -1,5 +1,13 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { PhaseRoutes } from "@/features/phase";
+import { StandBy } from "./StandBy";
+
 export const StandByRoutes= () => {
   return (
-    
+    <Routes>
+      <Route path='' element={ <StandBy /> } />
+      <Route path='games/*' element={ <PhaseRoutes /> } />
+      <Route path='*' element={ <Navigate to={'.'} /> } />     
+    </Routes>
   );
-}
+};
