74f8c293b0fbbce0ab08256788671a66aeb319f8
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-03 04:42:26 +0900

    fix : atomのkey変更

--- src/atoms/misc/modeSelectStates.ts
+++ src/atoms/misc/modeSelectStates.ts
@@ -3,11 +3,11 @@ import { MODES } from "@/features/modeSelect";
 import { ROOM_MODES } from "@/features/rooms";
 
 export const SelectedModeValueState= atom<string>({
-  key: 'selected-mode-value',
+  key: 'selecte-mode-value',
   default: MODES.TRAIN_MODE
 });
 
 export const RoomModeValueState= atom<string>({
-  key: 'room-mode-value',
+  key: 'room-select-mode-value',
   default: ROOM_MODES.CREATE_ROOM,
 });
