ba2ca413bddc6605f8db978d402c3f8e9ba1ba68
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-04 02:02:15 +0900

    add : react-queryの設定追加

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -2,8 +2,10 @@ import { QueryClient, DefaultOptions } from '@tanstack/react-query';
 
 const queryConfig: DefaultOptions= {
   queries: {
+    useErrorBoundary: true,
     refetchOnWindowFocus: false,
+    retry: false,
   },
 };
 
-export const queryClient: QueryClient = new QueryClient({defaultOptions: queryConfig});
+export const queryClient: QueryClient = new QueryClient({ defaultOptions: queryConfig });
