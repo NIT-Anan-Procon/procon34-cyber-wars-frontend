c0685b6b03e61211cdddf006fa17182f62dc80d4
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-05 02:51:11 +0900

    style : バトルフェーズのレイアウトのスタイリング

--- src/features/phase/routes/BattlePhase.tsx
+++ src/features/phase/routes/BattlePhase.tsx
@@ -1,5 +1,60 @@
+import styled from "styled-components";
+import { PhaseLayout, PhaseTimer, UserBoardsLayout } from "../components";
+
+import { EditArea } from "@/features/codeEdiror";
+import { colors } from "@/styles";
+
+const $PhaseTimer= styled(PhaseTimer)`
+  grid-column: 1 / span 2;
+  grid-row   : 1;
+  width      : 100vw;
+  height     : 10vh;
+`;
+
+const $MyUserScoreBoard= styled.div`
+  height: 20rem;
+  width : 40rem;
+  background: white;
+`;
+
+const $OpponentScoreBoard= styled.div`
+  height: 20rem;
+  width : 40rem;
+  background: white;
+`;
+
+const _Preview= styled.iframe`
+  grid-column: 1;
+  grid-row   : 3;
+  width      : 100%;
+  height     : 100%;
+  border     : none;
+  outline    : none;
+  background : white;
+`;
+
+const _EditorWrapper= styled.div`
+  grid-column: 2;
+  grid-row   : 3;
+  height: calc(100% - 10px);
+  width : 100%;
+  background: ${ colors.bgDarker };
+  font-size: 1.5rem;
+  overflow: auto;
+`;
+
 export const BattlePhase= () => {
   return (
-    <>BattlePhase</>
+    <PhaseLayout title='バトルフェーズ'>
+      <$PhaseTimer phaseTitle={'battle'}/>
+      <UserBoardsLayout>
+        <$MyUserScoreBoard />
+        <$OpponentScoreBoard />
+      </UserBoardsLayout>
+      <_EditorWrapper >
+        <EditArea />
+      </_EditorWrapper>
+      <_Preview src={'@/mocks/challengeData/mock_challenge.php'} />
+    </PhaseLayout>
   );
 };
