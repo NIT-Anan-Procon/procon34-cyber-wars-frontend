1d032645f8ca3e35c462052f62ef029f1a1112ff
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-05 15:20:56 +0900

    style : チェックボックスのスタイリング

--- src/components/Elements/CheckBoxList.tsx
+++ src/components/Elements/CheckBoxList.tsx
@@ -1,13 +1,82 @@
-import styled from 'styled-components';
+import { colors } from '@/assets/styles';
+import styled, { css } from 'styled-components';
 
 const _CheckBoxList= styled.ul`
-  
+  height: 100%;
+  width : 100%;
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(12rem, 1fr)
+  );
+  grid-template-rows: repeat(
+    auto-fill,
+    minmax(10rem, 1fr)
+  );
+`;
+
+const _CheckBoxItem= styled.li`
+  height: 5rem;
+  width: 10rem;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const _CheckBox= styled.input`
+  margin: 0;
+  padding: 0;
+  background: none;
+  border: none;
+  border-radius: 0;
+  outline: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  cursor: pointer;
+
+  &::before, 
+  &::after {
+    content: "";
+    display: block; 
+    position: relative;
+  }
+
+  &::before {
+    height: 5rem;
+    width : 10rem;
+    border-radius: 10px;
+    
+    ${(props) => props.checked
+      ? css`
+          box-shadow: 3px 3px 1.5px #a0e5dd, -3px -3px 1.5px #a0e5dd, -3px 3px 1.5px #a0e5dd, 3px -3px 1.5px #a0e5dd;
+          background: ${ colors.primary };
+        `
+      : css`
+        background: #141414;
+        `
+    }    
+  }
+`;
+
+const _CheckBoxLabel= styled.label`
+  font-size: 2rem;
+  z-index  : 10;
+  position: absolute;
+  top:50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  ${_CheckBox}:checked {
+    color:white;
+  }
 `;
 
 type CheckBoxListProps= {
-  label: string;
+  label?: string | undefined;
   values: Array<string>;
-  selected: string;
+  checked: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
@@ -15,7 +84,7 @@ export const CheckBoxList= (
   {     
     label, 
     values, 
-    selected, 
+    checked, 
     onChange, 
     ...props
   }: CheckBoxListProps
@@ -24,15 +93,19 @@ export const CheckBoxList= (
     <_CheckBoxList>
       { 
         values.map(( value, index ) => (
-          <li key={ index }>
-            <input 
+          <_CheckBoxItem key={ index }>
+            <_CheckBox 
+              id={`id_${index}`}
               type='checkbox'
               value={ value }
-              checked={ selected === value}
+              checked={ checked === value}
               onChange={ onChange }
               {...props} 
-            />           
-          </li>
+            />        
+            <_CheckBoxLabel htmlFor={`id_${index}`} >
+              { value }        
+            </_CheckBoxLabel>
+          </_CheckBoxItem>
         ))
       }
     </_CheckBoxList>
