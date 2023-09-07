f45c82c04d178e3e978f65b85ac3421a1bf81454
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-08 02:57:44 +0900

    add : 解説モードのルーティング追加

--- src/routes/modeRoutes.tsx
+++ src/routes/modeRoutes.tsx
@@ -1,11 +1,14 @@
+import { Explanation } from "@/features/explanation";
 import { PhaseRoutes } from "@/features/phase";
 import { Result } from "@/features/result";
 import { StandBy } from "@/features/standby";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-export const TrainRoutes= [
-
-];
+export const TrainRoutes= () => {
+  return (
+    <></>
+  );
+};
 
 export const GamesRoutes= () => {
   return (
@@ -13,8 +16,8 @@ export const GamesRoutes= () => {
       <Route path= { 'standby' }     element={ <StandBy /> } />
       <Route path= { 'phase/*' }     element={ <PhaseRoutes/> } />
       <Route path= { 'result' }      element={ <Result /> } />
-      <Route path= { 'explanation' } element={ <></> } />
-      <Route path= {'*'} element={ <Navigate to={''}/> } />
+      <Route path= { 'explanation' } element={ <Explanation /> } />
+      <Route path= {'*'} element={ <Navigate to='' /> } />
     </Routes>
   );
 };
