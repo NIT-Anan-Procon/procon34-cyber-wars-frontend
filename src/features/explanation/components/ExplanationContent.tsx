8d976e7da5a07408c222dcd688a84d5e1522bdca
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-09 05:19:07 +0900

    add : initial commit

---
+++ src/features/explanation/components/ExplanationContent.tsx
@@ -0,0 +1,9 @@
+type ExplanationContentProps= {
+  children: React.ReactNode;
+}
+
+export const ExplanationContent= ({ children }: ExplanationContentProps) => {
+  return (
+    <>{ children }</>
+  );
+};
