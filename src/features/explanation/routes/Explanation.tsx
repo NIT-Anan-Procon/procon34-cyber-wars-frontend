3022d8284a9c6ac54fe74cf58a1c76cf0a0e8ee1
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-09 05:18:47 +0900

    add : 解説画面のレイアウトをインポート

--- src/features/explanation/routes/Explanation.tsx
+++ src/features/explanation/routes/Explanation.tsx
@@ -1,9 +1,21 @@
-import { ContentHeader } from "@/components/Elements";
+import { useNavigate } from 'react-router-dom';
+
+import { ContentLayout }      from '@/components/Layout';
+import { Button }             from '@/components/Elements';
+import { ExplanationContent } from '../components';
 
 export const Explanation= () => {
+  const navigate= useNavigate();
+
   return (
-    <>
-      <ContentHeader title={'解説'} />
-    </>
+    <ContentLayout
+      headTitle= { '解説画面' }
+      header   = { '解説' }
+    >
+      <ExplanationContent>
+        
+      </ExplanationContent>
+      <Button type='button' onClick={() => navigate('../../')}>モード選択へ戻る</Button>      
+    </ContentLayout>
   );
 };
