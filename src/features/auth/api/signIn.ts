1545c63cc41c7032c4ab87a834984732f6a93fbe
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-09 21:33:02 +0900

    add : 認証の状態を更新するカスタムフックを使用

--- src/features/auth/api/signIn.ts
+++ src/features/auth/api/signIn.ts
@@ -1,17 +1,16 @@
 import { axios } from '@/lib/axios';
 import { AuthUser } from '..';
 import { SIGNIN_USER_URL } from '@/config/apiEndpoints';
-import { ISSUCCESS_KEY } from '@/config/responseKeys';
-import { useSetRecoilState } from 'recoil';
-import { isAuthState } from '@/atoms';
+import { useIsAuthenticated } from './hook/useIsAuthenticated';
+import { AxiosResponse } from 'axios';
 
 export const useSignIn= () => {
-  const setIsSuccessful= useSetRecoilState( isAuthState );
+  const { setIsAuthenticated }= useIsAuthenticated();
 
   async function isSignIn(data: AuthUser): Promise<void>{
     const resData= await axios.post( SIGNIN_USER_URL, data );
-    setIsSuccessful(resData[ISSUCCESS_KEY]);
-  }
+    setIsAuthenticated( resData );
+  };
 
   return { isSignIn };
-}
+};
--- src/features/auth/api/signUp.ts
+++ src/features/auth/api/signUp.ts
@@ -1,16 +1,14 @@
 import { axios } from '@/lib/axios';
 import { AuthUser } from '..';
 import { SIGNUP_USER_URL } from '@/config/apiEndpoints';
-import { useSetRecoilState } from 'recoil';
-import { isAuthState } from '@/atoms';
-import { ISSUCCESS_KEY } from '@/config/responseKeys';
+import { useIsAuthenticated } from './hook/useIsAuthenticated';
 
 export const useSignUp= () => {
-  const setIsSuccessful= useSetRecoilState( isAuthState );
+  const { setIsAuthenticated }= useIsAuthenticated();
 
   async function isSignUp( data: AuthUser ): Promise<void>{
     const resData= await axios.post( SIGNUP_USER_URL, data );
-    setIsSuccessful( resData[ISSUCCESS_KEY] );
+    setIsAuthenticated( resData );
   }
 
   return { isSignUp };
