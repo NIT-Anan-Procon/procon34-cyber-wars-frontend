d3314fcf15fe1c90beb0ee82ea52c3b9e067807f
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-09 20:04:04 +0900

    fix : atomにレスポンスデータを更新する

--- src/features/auth/api/signIn.ts
+++ src/features/auth/api/signIn.ts
@@ -1,9 +1,17 @@
-import { AxiosResponse } from 'axios';
-
-import { axios }           from '@/lib/axios';
-import { AuthUser }        from '..';
+import { axios } from '@/lib/axios';
+import { AuthUser } from '..';
 import { SIGNIN_USER_URL } from '@/config/apiEndpoints';
+import { ISSUCCESS_KEY } from '@/config/responseKeys';
+import { useSetRecoilState } from 'recoil';
+import { isAuthState } from '@/atoms';
 
-export const signIn= ( data: AuthUser ): Promise<AxiosResponse<boolean>> => {
-  return axios.post( SIGNIN_USER_URL, data );
-};
+export const useSignIn= () => {
+  const setIsSuccessful= useSetRecoilState( isAuthState );
+
+  async function isSignIn(data: AuthUser): Promise<void>{
+    const resData= await axios.post( SIGNIN_USER_URL, data );
+    setIsSuccessful(resData[ISSUCCESS_KEY]);
+  }
+
+  return { isSignIn };
+}
--- src/features/auth/api/signUp.ts
+++ src/features/auth/api/signUp.ts
@@ -1,9 +1,17 @@
-import { AxiosResponse } from 'axios';
-
-import { axios }           from '@/lib/axios';
-import { AuthUser }        from '..';
+import { axios } from '@/lib/axios';
+import { AuthUser } from '..';
 import { SIGNUP_USER_URL } from '@/config/apiEndpoints';
+import { useSetRecoilState } from 'recoil';
+import { isAuthState } from '@/atoms';
+import { ISSUCCESS_KEY } from '@/config/responseKeys';
 
-export const signUp= ( data: AuthUser ): Promise<AxiosResponse<boolean>> => {
-  return axios.post( SIGNUP_USER_URL, data );
-};
+export const useSignUp= () => {
+  const setIsSuccessful= useSetRecoilState( isAuthState );
+
+  async function isSignUp( data: AuthUser ): Promise<void>{
+    const resData= await axios.post( SIGNUP_USER_URL, data );
+    setIsSuccessful( resData[ISSUCCESS_KEY] );
+  }
+
+  return { isSignUp };
+}
