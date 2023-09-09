993fa599c134a7d068de840d512607bd12a82ef5
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-09 20:54:45 +0900

    refactor : インポート修正

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,16 @@
-import { useRoutes } from 'react-router-dom';
+import { useRoutes }      from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 
 import { Landing } from '@/features/misc';
 
 import { authorizedRoutes } from './authorizedRoutes';
-import { publicRoutes } from './publicRoutes';
-import { useRecoilValue } from 'recoil';
+import { publicRoutes }     from './publicRoutes';
+
 import { isAuthState } from '@/atoms';
 
 export const AppRoutes = () => {
   const isAuthenticated= useRecoilValue<boolean>(isAuthState);
-
+  
   const commonRoutes= [{ path: '/', element: <Landing /> }];
   
   const routes = isAuthenticated ? authorizedRoutes : publicRoutes;
