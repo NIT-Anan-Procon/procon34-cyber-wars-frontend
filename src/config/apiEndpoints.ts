d9bbdb0f1a5df2727436a0888edcf18d92222b8e
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-11 22:29:40 +0900

    refactor : 可読性向上のため、spacing

--- src/config/apiEndpoints.ts
+++ src/config/apiEndpoints.ts
@@ -1,9 +1,9 @@
-export const SIGNUP_USER_URL= import.meta.env.VITE_API_URL_USER_SIGNUP;
-export const UPDATE_USER_NAME_URL= import.meta.env.VITE_API_URL_USER_UPDATE_NAME;
+export const SIGNUP_USER_URL         = import.meta.env.VITE_API_URL_USER_SIGNUP;
+export const UPDATE_USER_NAME_URL    = import.meta.env.VITE_API_URL_USER_UPDATE_NAME;
 export const UPDATE_USER_PASSWORD_URL= import.meta.env.VITE_API_URL_USER_UPDATE_PASSWORD;
-export const SIGNIN_USER_URL = import.meta.env.VITE_API_URL_USER_SIGNIN;
-export const IS_SIGNEDIN_URL = import.meta.env.VITE_API_URL_USER_ISSIGNEDIN;
-export const SIGNOUT_USER_URL= import.meta.env.VITE_API_URL_USER_SIGNOUT;
+export const SIGNIN_USER_URL         = import.meta.env.VITE_API_URL_USER_SIGNIN;
+export const IS_SIGNEDIN_URL         = import.meta.env.VITE_API_URL_USER_ISSIGNEDIN;
+export const SIGNOUT_USER_URL        = import.meta.env.VITE_API_URL_USER_SIGNOUT;
 
 export const ROOM_URL  = import.meta.env.VITE_API_URL_ROOM;
 
