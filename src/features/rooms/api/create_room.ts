bf878758ed7f026dce4fa217d08a9b32540df25f
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-11 18:16:31 +0900

    fix : ルーム関連のapiのurlを修正

--- src/features/rooms/api/create_room.ts
+++ src/features/rooms/api/create_room.ts
@@ -1,8 +1,8 @@
 import { AxiosResponse } from 'axios';
 
 import { axios } from '@/lib/axios';
-import { CREATE_ROOM_URL } from '@/config/apiEndpoints';
+import { ROOM_URL } from '@/config/apiEndpoints';
 
 export const createRoom= ( isDifficult: boolean ): Promise<AxiosResponse<number>> => {
-  return axios.post( CREATE_ROOM_URL, isDifficult );
+  return axios.post( ROOM_URL, isDifficult );
 };
