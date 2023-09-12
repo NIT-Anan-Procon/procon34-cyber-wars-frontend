3645fa9faee7b4075821bb038ecf59e0b1a2d2c2
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-12 13:17:28 +0900

    refactor : 不要なコンポーネントの削除

--- src/features/phase/routes/AttackPhase.tsx
+++ src/features/phase/routes/AttackPhase.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { 
+  PhaseContents,
   PhaseLayout, 
   PhaseTimer, 
   UserBoardsLayout,
@@ -9,15 +10,9 @@ import {
 import { colors } from "@/assets/styles";
 import { Button, CheckBoxList } from "@/components/Elements";
 import { useAtomValueChange } from '@/hooks/useAtomValueChange';
-import { vulnerabilityOptionState } from '@/atoms/game';
+import { vulnerabilityOptionsState } from '@/atoms/game';
 import { useNavigate } from 'react-router-dom';
-
-const $PhaseTimer= styled(PhaseTimer)`
-  grid-column: 1 / span 2;
-  grid-row   : 1;
-  width      : 100vw;
-  height     : 10vh;
-`;
+import { PHASE } from '../types';
 
 const $MyUserScoreBoard= styled(UserScoreBoard)`
   height    : 100%;
@@ -29,16 +24,6 @@ const $OpponentScoreBoard= styled(UserScoreBoard)`
   height    : 100%;
   width     : 40rem;
   background: white;
-`;
-
-const _Preview= styled.iframe`
-  grid-column: 1;
-  grid-row   : 3;
-  width      : 100%;
-  height     : 100%;
-  border     : none;
-  outline    : none;
-  background : white;
 `;
 
 const _EditorWrapper= styled.div`
@@ -54,43 +39,13 @@ const _EditorWrapper= styled.div`
   overflow      : auto;
 `;
 
-const _EditorHead= styled.div`
-  height: 10rem;
-  width : 100%;
-  padding-left: 3.5rem;
-  display: flex;
-  align-items: center;
-  background:#000007;
-
-  > span {
-    font-size: 1.8rem;
-    font-weight: bolder;
-    color : ${ colors.primary };
-  }
-`;
-
-const _EditorFoot= styled.div`
-  height: 8rem;
-  width : 100%;
-  display: flex;
-  justify-content: end;
-  position: relative;
-  bottom  : 0;
-  background: #000007;
-`;
-
-const $SendButton= styled(Button)`
-  height: 5rem;
-  width: 20rem;
-`;
-
 export const AttackPhase= () => {
   const navigate= useNavigate();
-  const [ checkedOption, updateCheckedOption ]= useAtomValueChange(vulnerabilityOptionState);
+  const [ checkedOption, updateCheckedOption ]= useAtomValueChange(vulnerabilityOptionsState);
 
   return (
     <PhaseLayout title='アタックフェーズ'>
-      <$PhaseTimer phaseTitle={'attack'}/>
+      <PhaseTimer phaseTitle={ PHASE.ATTACK_PHASE }/>
       <UserBoardsLayout>
         <$MyUserScoreBoard 
           name  = {'日下 遥斗'}
@@ -103,20 +58,15 @@ export const AttackPhase= () => {
           score = { 100 }           
         />
       </UserBoardsLayout>
-      <_EditorWrapper >
-        <_EditorHead >
-          <span>ソースコードを並べ替えて攻撃文を完成させてください</span>
-        </_EditorHead>
-        <CheckBoxList
-          values ={['"', '1', 'OR', '1', '=']}
-          checked={ checkedOption }
-          onChange={ updateCheckedOption }
-        />
-        <_EditorFoot>
-          <$SendButton type='button' onClick={() => navigate('../defence-phase')} >Send</$SendButton>
-        </_EditorFoot>
-      </_EditorWrapper>
-      <_Preview src={'@/mocks/challengeData/mock_challenge.php'} />
+      <PhaseContents phaseTitle={ PHASE.DEFENCE_PHASE } >
+        <_EditorWrapper>
+          <CheckBoxList
+            values ={['"', '1', 'OR', '1', '=']}
+            checked={ checkedOption }
+            onChange={ updateCheckedOption }
+          />
+        </_EditorWrapper>         
+      </PhaseContents>
     </PhaseLayout>
   );
 };
--- src/features/phase/routes/DefencePhase.tsx
+++ src/features/phase/routes/DefencePhase.tsx
@@ -1,22 +1,15 @@
 import styled from 'styled-components';
 import { 
+  PhaseContents,
   PhaseLayout, 
   PhaseTimer, 
   UserBoardsLayout,
   UserScoreBoard
 } from "../components";
 
-import { colors } from "@/assets/styles";
-import { Button } from "@/components/Elements";
 import { useNavigate } from 'react-router-dom';
-import { EditArea } from '@/features/codeEdiror';
-
-const $PhaseTimer= styled(PhaseTimer)`
-  grid-column: 1 / span 2;
-  grid-row   : 1;
-  width      : 100vw;
-  height     : 10vh;
-`;
+import { EditorForm } from '@/features/code';
+import { DESCRIPTIONS, PHASE } from '../types';
 
 const $MyUserScoreBoard= styled(UserScoreBoard)`
   height    : 100%;
@@ -30,16 +23,6 @@ const $OpponentScoreBoard= styled(UserScoreBoard)`
   background: white;
 `;
 
-const _Preview= styled.iframe`
-  grid-column: 1;
-  grid-row   : 3;
-  width      : 100%;
-  height     : 100%;
-  border     : none;
-  outline    : none;
-  background : white;
-`;
-
 const _EditorWrapper= styled.div`
   grid-column   : 2;
   grid-row      : 3;
@@ -48,39 +31,9 @@ const _EditorWrapper= styled.div`
   display       : flex;
   flex-direction: column;
   row-gap       : 2rem;
-  background    : #000007;
+  background    : transparent;
   font-size     : 1.5rem;
   overflow      : auto;
-`;
-
-const _EditorHead= styled.div`
-  height: 10rem;
-  width : 100%;
-  padding-left: 3.5rem;
-  display: flex;
-  align-items: center;
-  background:#000007;
-
-  > span {
-    font-size: 1.8rem;
-    font-weight: bolder;
-    color : ${ colors.primary };
-  }
-`;
-
-const _EditorFoot= styled.div`
-  height: 8rem;
-  width : 100%;
-  display: flex;
-  justify-content: end;
-  position: relative;
-  bottom  : 0;
-  background: #000007;
-`;
-
-const $SendButton= styled(Button)`
-  height: 5rem;
-  width: 20rem;
 `;
 
 export const DefencePhase= () => {
@@ -88,7 +41,7 @@ export const DefencePhase= () => {
   
   return (
     <PhaseLayout title='ディフェンスフェーズ'>
-      <$PhaseTimer phaseTitle={'defence'}/>
+      <PhaseTimer phaseTitle={ PHASE.DEFENCE_PHASE }/>
       <UserBoardsLayout>
         <$MyUserScoreBoard 
           name  = {'日下 遥斗'}
@@ -101,16 +54,14 @@ export const DefencePhase= () => {
           score = { 100 }           
         />
       </UserBoardsLayout>
-      <_EditorWrapper >
-        <_EditorHead >
-          <span>ソースコードを並べ替えて攻撃文を完成させてください</span>
-        </_EditorHead>
-          <EditArea />
-        <_EditorFoot>
-          <$SendButton type='button' onClick={() => navigate('../battle-phase')} >Send</$SendButton>
-        </_EditorFoot>
-      </_EditorWrapper>
-      <_Preview src={'@/mocks/challengeData/mock_challenge.php'} />
+      <PhaseContents phaseTitle={ PHASE.DEFENCE_PHASE } >
+        <_EditorWrapper >
+          <EditorForm 
+            isHint = { false }
+            navText={ DESCRIPTIONS.DEFENCE_PHASE }
+          />
+        </_EditorWrapper>        
+      </PhaseContents>
     </PhaseLayout>
   );
 };
