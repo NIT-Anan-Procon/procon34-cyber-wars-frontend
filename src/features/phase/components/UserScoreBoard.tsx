d168e37f204f160378cea1505e369521fdbf6bfd
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-13 00:11:00 +0900

    style : ユーザ表示のスタイル修正

--- src/features/phase/components/UserScoreBoard.tsx
+++ src/features/phase/components/UserScoreBoard.tsx
@@ -1,121 +1,146 @@
 import styled, { css } from 'styled-components';
+import attackPhase from '@/assets/images/attack_phase.svg'
 
 const _UserBoardWrapper= styled.div`
-  height: 100%;
-  width : 40rem;
-  position  : relative;
-  background: transparent;
+  height     : 100%;
+  width      : 40vw;
+  padding-top: 10px;
+  position   : relative; 
+  
+  ${(props) => props.status === 'HOST'
+    ? css`
+        &::before {
+          height    : 100%;
+          width     : 100%;
+          content   : '';
+          position  : absolute;
+          clip-path : polygon(0 0, 100% 0%, 75% 100%, 0% 100%);
+          background: #2F1FF6;
+        }
+      `
+    : css`
+        &::before {
+          height    : 100%;
+          width     : 100%;
+          content   : '';
+          position  : absolute;
+          clip-path : polygon(25% 0, 100% 0%, 100% 100%, 0% 100%);
+          background: #CA1C1C;
+        }
+      `
+  };
 `;
 
 const _UserIconArea= styled.div`
-  height: 100%;
-  width : 20rem;
-  position  : absolute;
-  background: #000000;
-  z-index: 10;
+  height         : 15rem;
+  width          : 15rem;
+  display        : flex;
+  justify-content: center;
+  position       : absolute;
+  bottom         : 10px;
+  background     : #d4d4d4;
+  z-index        : 100;
+
   ${(props) => props.status === 'HOST'
-      ? css` left : 0px`
-      : css` right: 0px`
+      ? css` 
+          left : 2rem;
+          transform: rotate(-5deg);
+          &::before {
+            content   : '';
+            height    : 100%;
+            width     : 100%;
+            background: black;
+            z-index   : -100;
+          }
+        `
+      : css` 
+          right: 2rem;
+          transform: rotate(5deg);
+          &::before {
+            content   : '';
+            height    : 100%;
+            width     : 100%;
+            background: black;
+            z-index   : -100;
+          }
+        `
   }  
 `;
 
 const _UserIcon= styled.img`
-  width  : 100%;
-  display: flex;
+  width      : 100%;
+  display    : flex;
   align-items: center;
   justify-content: center;
 `;
 
 const _UserScoreWrapper= styled.div`
-  height: 8rem;
-  width : calc(100% - 10px);
-  display: flex;
+  height     : 10rem;
+  width      : calc(100% - 10px);
+  display    : flex;
   align-items: center;        
-  position  : relative;
-  background: black;
+  position   : relative;
+  background : black;
+  
   ${(props) => props.status === 'HOST'
     ? css`
-        padding-right: 20px;  
+        padding-right  : 20px;  
         justify-content: end;
-        transform: rotate(-5deg);
-        box-shadow: 8px 8px #2F1FF6;
+        box-shadow     : 8px 8px #2F1FF6;
       `
     : css`
-        padding-left: 20px;  
+        padding-left   : 20px;  
         justify-content: start;
-        transform: rotate(5deg);
-        box-shadow: 8px 8px #CA1C1C;
+        box-shadow     : 8px 8px #CA1C1C;
       `
   }
 `;
 
 const _UserScore= styled.h1`
-  position: absolute;
+  position : absolute;
   font-size: 5rem;
-  color: white;
-  z-index: 10;
+  color    : white;
+  z-index  : 10;
 
   > span {
     margin-left: 10px;
-    font-size: 3rem;
+    font-size  : 3rem;
   }
 `;
 
 const _UserNameWrapper= styled.div`
-  position : relative;
-  bottom   : 0;
-
-  &::before {
-    position: absolute;
-    content : '';
-    height  : 7.5rem;
-    width   : 70%;
-    
-    ${(props) => props.status === 'HOST'
-      ? css`
-        left: 0;
-        background: #2F1FF6`
-      : css`
-        right: 0;
-        background: #CA1C1C`
-    };
-  }  
+  position  : relative;
+  height    : 6.5rem;
+  width     : 20rem;
+  display   : flex;
+  align-items: center;
+  justify-content: center;
+  background: black;
+  z-index   : 100;
+  
   &::after {
-    background: black;
-    height: 6.5rem;
-    width : calc(70% - 3rem);
-    display: block;
-    position: absolute;
-    content: '';
-    z-index: 100;
-
     ${(props) => props.status === 'HOST'
       ? css`
-        left: 0;
-        clip-path: polygon(0 70%, 100% 0, 100% 100%, 0% 100%)`
+          clip-path: polygon(0 70%, 100% 0, 100% 100%, 0% 100%)
+        `
       : css`
-        right: 0;
-        clip-path: polygon(0 0, 100% 70%, 100% 100%, 0% 100%);`
+          clip-path: polygon(0 0, 100% 70%, 100% 100%, 0% 100%);
+        `  
     }
   }
 `;
 
 const _UserName= styled.h1`
-  position : absolute;
   font-size: 3.5rem;
   color    : white;
   z-index  : 999;
 
   ${(props) => props.status === 'HOST'
       ? css`
-          top  : 0px;  
-          right: 10%;
-          transform: translateX(-100%) rotate(-5deg);
+          transform: rotate(-5deg);
         `
       : css`
-        top : 0px;  
-        left: 10%;
-        transform: translateX(100%) rotate(5deg);
+          transform: rotate(5deg);
         `
     }
 `;
@@ -134,11 +159,11 @@ export const UserScoreBoard= (
   }: UserScoreBoardProps
 ) => {
   return (
-    <_UserBoardWrapper>
+    <_UserBoardWrapper status={status} >
       <_UserIconArea status={status} >
         <_UserIcon 
-          src={''}
-          alt=''
+          src={attackPhase}
+          alt= { 'icon' }
         />
       </_UserIconArea>
       <_UserScoreWrapper status={status}>
