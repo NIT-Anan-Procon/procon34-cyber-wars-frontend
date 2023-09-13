a3c07c6991a567ef336b5be5ad89075683c81a30
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-13 23:41:10 +0900

    add : コードを非同期で取得する

--- src/features/code/api/fetchCode.ts
+++ src/features/code/api/fetchCode.ts
@@ -1,6 +1,6 @@
 import { DEFENCE_CODE_URL } from "@/config/apiEndpoints";
 import { axios } from "@/lib/axios"
 
-export const fetchCode= () => {
-  return axios.get( DEFENCE_CODE_URL );
+export const fetchCode= async(): Promise<string> => {
+  return await axios.get( DEFENCE_CODE_URL );
 };
--- src/features/code/api/fetchRevisionCode.ts
+++ src/features/code/api/fetchRevisionCode.ts
@@ -1,6 +1,6 @@
 import { BATTLE_REVISION_URL } from '@/config/apiEndpoints';
 import { axios } from '@/lib/axios';
 
-export const fetchRevisionCode= () => {
-  return axios.get( BATTLE_REVISION_URL );
+export const fetchRevisionCode= async(): Promise<string> => {
+  return await axios.get( BATTLE_REVISION_URL );
 }
