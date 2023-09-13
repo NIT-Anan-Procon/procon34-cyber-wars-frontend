cee4b16bcbe16fd1949250027f2794552da2b134
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-13 23:42:51 +0900

    add : コード取得先のハンドリング

--- src/features/code/selector/updateCodeSelector.ts
+++ src/features/code/selector/updateCodeSelector.ts
@@ -1,15 +1,24 @@
-import { selector } from 'recoil';
+import { selectorFamily } from 'recoil';
 
 import { codeState } from '@/atoms';
 import { fetchCode } from '../api/fetchCode';
+import { CODE_KEY } from '@/config/responseKeys';
+import { PHASE } from '@/features/phase/types';
+import { fetchRevisionCode } from '../api/fetchRevisionCode';
 
-export const updateCodeSelector= selector({
+export const updateCodeSelector= selectorFamily({
   key: 'selector_ updateCodeSelector',
-  get: async() => {
-    const codeData= await fetchCode();
-    return codeData;
+  get: ( phase ) => async() => {
+    if( phase === PHASE.DEFENCE_PHASE ) {
+      const codeData= await fetchCode();
+      return codeData[ CODE_KEY ];
+
+    } else {
+      const codeRevisionData= await fetchRevisionCode();
+      return codeRevisionData[ CODE_KEY ];
+    }
   },
-  set: ({ set }, newCodeValue) => {
+  set: () => ({ set }, newCodeValue) => {
     set( codeState, newCodeValue );
   }
 });
