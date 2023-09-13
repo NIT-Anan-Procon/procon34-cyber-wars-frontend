f77a71e55387d3d7c33127d49260a36ea088c5eb
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-13 23:40:14 +0900

    refactor : 変数名の修正

--- src/config/apiEndpoints.ts
+++ src/config/apiEndpoints.ts
@@ -7,13 +7,13 @@ export const SIGNOUT_USER_URL        = import.meta.env.VITE_API_URL_USER_SIGNOUT
 
 export const ROOM_URL  = import.meta.env.VITE_API_URL_ROOM;
 
-export const START_GAME_URL         = import.meta.env.VITE_API_URL_GAME_START;
-export const GET_GAME_START_TIME_URL= import.meta.env.VITE_API_URL_GAME_START_TIME;
-export const GAME_OPPONENT_NAME_URL = import.meta.env.VITE_API_URL_GAME_OPPONENT_NAME;
-export const GAME_SCORES_URL        = import.meta.env.VITE_API_URL_GAME_SCORES;
-export const ATTACK_CHALLENGE_URL   = import.meta.env.VITE_API_URL_GAME_ATTACK_CHALLENGE;
-export const ATTACK_HINT_URL        = import.meta.env.VITE_API_URL_GAME_ATTACK_HINT;
-export const ATTACK_SEND_KEY_URL    = import.meta.env.VITE_API_URL_GAME_ATTACK_SEND_KEY;
-export const DEFENCE_CODE_URL       = import.meta.env.VITE_API_URL_GAME_DEFENCE_CODE;
-export const BATTLE_REVISION_URL    = import.meta.env.VITE_API_URL_GAME_BATTLE_REVISION;
-export const BATTLE_SEND_KEY_URL    = import.meta.env.VITE_API_URL_GAME_BATTLE_SEND_KEY;
+export const START_GAME_URL           = import.meta.env.VITE_API_URL_GAME_START;
+export const FETCH_GAME_START_TIME_URL= import.meta.env.VITE_API_URL_GAME_START_TIME;
+export const GAME_OPPONENT_NAME_URL   = import.meta.env.VITE_API_URL_GAME_OPPONENT_NAME;
+export const GAME_SCORES_URL          = import.meta.env.VITE_API_URL_GAME_SCORES;
+export const ATTACK_CHALLENGE_URL     = import.meta.env.VITE_API_URL_GAME_ATTACK_CHALLENGE;
+export const ATTACK_HINT_URL          = import.meta.env.VITE_API_URL_GAME_ATTACK_HINT;
+export const ATTACK_SEND_KEY_URL      = import.meta.env.VITE_API_URL_GAME_ATTACK_SEND_KEY;
+export const DEFENCE_CODE_URL         = import.meta.env.VITE_API_URL_GAME_DEFENCE_CODE;
+export const BATTLE_REVISION_URL      = import.meta.env.VITE_API_URL_GAME_BATTLE_REVISION;
+export const BATTLE_SEND_KEY_URL      = import.meta.env.VITE_API_URL_GAME_BATTLE_SEND_KEY;
