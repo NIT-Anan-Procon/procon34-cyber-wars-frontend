cea4baeebbcc8c15cad33e5ad69c489adf65682a
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-14 13:14:39 +0900

    add : 非同期でコード送信を行うカスタムフックを作成

--- src/features/code/api/sendCode.ts
+++ src/features/code/api/sendCode.ts
@@ -1,6 +1,20 @@
-import { DEFENCE_CODE_URL } from "@/config/apiEndpoints";
-import { axios } from "@/lib/axios"
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
-export const sendCode = () => {
-  return axios.put( DEFENCE_CODE_URL );
-}
+import { axios }            from '@/lib/axios';
+import { DEFENCE_CODE_URL } from '@/config/apiEndpoints';
+import { CODE_KEY, IS_VALID_KEY }  from '@/config/responseKeys';
+import { codeState, isValidState } from '@/atoms';
+
+export const useSendCode = () => {
+  const currentCode= useRecoilValue( codeState ); 
+  const setIsValid = useSetRecoilState( isValidState );
+
+  async function sendCode(): Promise<void> {
+    const sendData= JSON.stringify({ [ CODE_KEY ]: currentCode });
+    const resData = await axios.put( DEFENCE_CODE_URL, sendData );
+
+    setIsValid( resData[ IS_VALID_KEY ] );
+  };
+
+  return { sendCode };
+};
