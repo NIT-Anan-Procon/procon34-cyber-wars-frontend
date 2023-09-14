abf5452158569f38c4a72d1f0a66c9c679b6cbd4
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-14 13:15:54 +0900

    add : フォーム送信するデータのprops追加

--- src/features/phase/routes/BattlePhase.tsx
+++ src/features/phase/routes/BattlePhase.tsx
@@ -10,17 +10,12 @@ import {
 import { useNavigate } from 'react-router-dom';
 import { EditorForm } from '@/features/code';
 import { DESCRIPTIONS, PHASE } from '../types';
+import { useSendCode } from '@/features/code/api/sendCode';
 
-const $MyUserScoreBoard= styled(UserScoreBoard)`
-  height    : 100%;
-  width     : 40rem;
-  background: white;
-`;
-
-const $OpponentScoreBoard= styled(UserScoreBoard)`
-  height    : 100%;
-  width     : 40rem;
-  background: white;
+const _PhaseHead= styled.div`
+  height: 30vh;
+  width : 100%;
+  display: flex;
 `;
 
 const _EditorWrapper= styled.div`
@@ -38,27 +33,32 @@ const _EditorWrapper= styled.div`
 
 export const BattlePhase= () => {
   const navigate= useNavigate();
+  const { sendCode }= useSendCode();
   
   return (
     <PhaseLayout title='バトルフェーズ'>
-      <PhaseTimer phaseTitle={ PHASE.BATTLE_PHASE }/>
-      <UserBoardsLayout>
-        <$MyUserScoreBoard 
-          name  = {'日下 遥斗'}
-          status= { 'HOST' }
-          score = { 100 } 
-        />
-        <$OpponentScoreBoard 
-          name  = {'木下 聡大'}
-          status= { 'GUEST' }
-          score = { 100 }           
-        />
-      </UserBoardsLayout>
+      <_PhaseHead>
+        <UserBoardsLayout>
+          <UserScoreBoard 
+            name  = {'日下 遥斗'}
+            status= { 'HOST' }
+            score = { 100 } 
+          /> 
+          <PhaseTimer phaseTitle={ PHASE.BATTLE_PHASE }/>
+          <UserScoreBoard 
+            name  = {'木下 聡大'}
+            status= { 'GUEST' }
+            score = { 100 }           
+          />
+        </UserBoardsLayout>      
+      </_PhaseHead>
       <PhaseContents phaseTitle={ PHASE.BATTLE_PHASE } >
         <_EditorWrapper >
           <EditorForm 
             isHint = { false }
-            navText={ DESCRIPTIONS.BATTLE_PHASE }
+            navText= { DESCRIPTIONS.BATTLE_PHASE }
+            phase  = { PHASE.BATTLE_PHASE }
+            submitData={ sendCode }
           />
         </_EditorWrapper>        
       </PhaseContents>
--- src/features/phase/routes/DefencePhase.tsx
+++ src/features/phase/routes/DefencePhase.tsx
@@ -10,17 +10,12 @@ import {
 import { useNavigate } from 'react-router-dom';
 import { EditorForm } from '@/features/code';
 import { DESCRIPTIONS, PHASE } from '../types';
+import { useSendCode } from '../../code/api/sendCode';
 
-const $MyUserScoreBoard= styled(UserScoreBoard)`
-  height    : 100%;
-  width     : 40rem;
-  background: white;
-`;
-
-const $OpponentScoreBoard= styled(UserScoreBoard)`
-  height    : 100%;
-  width     : 40rem;
-  background: white;
+const _PhaseHead= styled.div`
+  height: 30vh;
+  width : 100%;
+  display: flex;
 `;
 
 const _EditorWrapper= styled.div`
@@ -38,27 +33,32 @@ const _EditorWrapper= styled.div`
 
 export const DefencePhase= () => {
   const navigate= useNavigate();
+  const { sendCode }= useSendCode();
   
   return (
     <PhaseLayout title='ディフェンスフェーズ'>
-      <PhaseTimer phaseTitle={ PHASE.DEFENCE_PHASE }/>
-      <UserBoardsLayout>
-        <$MyUserScoreBoard 
-          name  = {'日下 遥斗'}
-          status= { 'HOST' }
-          score = { 100 } 
-        />
-        <$OpponentScoreBoard 
-          name  = {'木下 聡大'}
-          status= { 'GUEST' }
-          score = { 100 }           
-        />
-      </UserBoardsLayout>
+      <_PhaseHead>
+        <UserBoardsLayout>
+          <UserScoreBoard 
+            name  = {'日下 遥斗'}
+            status= { 'HOST' }
+            score = { 100 } 
+          /> 
+          <PhaseTimer phaseTitle={ PHASE.DEFENCE_PHASE }/>
+          <UserScoreBoard 
+            name  = {'木下 聡大'}
+            status= { 'GUEST' }
+            score = { 100 }           
+          />
+        </UserBoardsLayout>      
+      </_PhaseHead>
       <PhaseContents phaseTitle={ PHASE.DEFENCE_PHASE } >
         <_EditorWrapper >
           <EditorForm 
             isHint = { false }
-            navText={ DESCRIPTIONS.DEFENCE_PHASE }
+            navText= { DESCRIPTIONS.DEFENCE_PHASE }
+            phase  = { PHASE.DEFENCE_PHASE }
+            submitData={ sendCode }
           />
         </_EditorWrapper>        
       </PhaseContents>
