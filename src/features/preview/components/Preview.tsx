d034dae580acf4d0714271bc8a938b5dcb9fe130
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-14 13:23:06 +0900

    add : セレクタで生成された絶対パスを追加

--- src/features/preview/components/Preview.tsx
+++ src/features/preview/components/Preview.tsx
@@ -1,4 +1,7 @@
+import { codeState } from '@/atoms';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
+import { createAbsolutePath } from '../selector/createAbsolutePath';
 
 const _PreviewBox= styled.div<{ styles?: string }>`
   width      : 100%;
@@ -11,7 +14,7 @@ const _PreviewBox= styled.div<{ styles?: string }>`
 `;
 
 const _Preview= styled.iframe`
-    width      : 100%;
+  width      : 100%;
   height     : 100%;
 `;
 
@@ -24,39 +27,14 @@ type PreviewProps= {
 export const Preview= (
   { 
     phaseTitle,
-    codePath,
     styles,  
   }: PreviewProps
 ) => {
+  const getCodePath= useRecoilValue( createAbsolutePath(phaseTitle) );
+
   return (
-    <_PreviewBox styles={styles} >
-      <_Preview srcDoc={`<?php
-// セッションを開始する
-session_start();
-
-// セッション変数にユーザー名が保存されているかチェックする
-if (!isset($_SESSION['username'])) {
-  // 保存されていない場合はログインページにリダイレクトする
-  header('Location: login.php');
-  exit;
-}
-
-// セッション変数からユーザー名を取得する
-$username = $_SESSION['username'];
-?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <title>ログイン成功ページ</title>
-</head>
-<body>
-  <h1>ログイン成功ページ</h1>
-  <!-- ユーザー名を表示する -->
-  <p><?php echo $username; ?>さん、こんにちは！</p>
-</body>
-</html>`} />
+    <_PreviewBox styles={ styles } >
+      <_Preview src= { getCodePath } /> 
     </_PreviewBox>
   );
 };
