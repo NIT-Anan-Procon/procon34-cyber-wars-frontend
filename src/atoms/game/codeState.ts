c8d2d9243c5d3871d88671eb657bd2882a95e2db
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-15 03:02:53 +0900

    add : 修正後のコードの状態を保持するatomを追加

--- src/atoms/game/codeState.ts
+++ src/atoms/game/codeState.ts
@@ -4,3 +4,8 @@ export const codeState= atom<string>({
   key: 'atom_codeState',
   default: ''
 });
+
+export const revisionCodeState= atom<string>({
+  key: 'atom_revisionCodeState',
+  default: ''
+});
