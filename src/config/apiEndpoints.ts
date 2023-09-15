84616ef09283e59959e1c01417005372477b16f9
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-15 15:46:23 +0900

    add : エンドポイント設定

--- src/config/apiEndpoints.ts
+++ src/config/apiEndpoints.ts
@@ -1,19 +1,19 @@
-export const SIGNUP_USER_URL         = import.meta.env.VITE_API_URL_USER_SIGNUP;
-export const UPDATE_USER_NAME_URL    = import.meta.env.VITE_API_URL_USER_UPDATE_NAME;
-export const UPDATE_USER_PASSWORD_URL= import.meta.env.VITE_API_URL_USER_UPDATE_PASSWORD;
-export const SIGNIN_USER_URL         = import.meta.env.VITE_API_URL_USER_SIGNIN;
-export const IS_SIGNEDIN_URL         = import.meta.env.VITE_API_URL_USER_ISSIGNEDIN;
-export const SIGNOUT_USER_URL        = import.meta.env.VITE_API_URL_USER_SIGNOUT;
+export const SIGNUP_USER_URL         = '/user';
+export const UPDATE_USER_NAME_URL    = '/user/name';
+export const UPDATE_USER_PASSWORD_URL= '/user/password';
+export const SIGNIN_USER_URL         = '/user/credential';
+export const IS_SIGNEDIN_URL         = '/user/credential';
+export const SIGNOUT_USER_URL        = '/user/credential';
 
-export const ROOM_URL  = import.meta.env.VITE_API_URL_ROOM;
+export const ROOM_URL  = '/room';
 
-export const START_GAME_URL           = import.meta.env.VITE_API_URL_GAME_START;
-export const FETCH_GAME_START_TIME_URL= import.meta.env.VITE_API_URL_GAME_START_TIME;
-export const GAME_OPPONENT_NAME_URL   = import.meta.env.VITE_API_URL_GAME_OPPONENT_NAME;
-export const GAME_SCORES_URL          = import.meta.env.VITE_API_URL_GAME_SCORES;
-export const ATTACK_CHALLENGE_URL     = import.meta.env.VITE_API_URL_GAME_ATTACK_CHALLENGE;
-export const ATTACK_HINT_URL          = import.meta.env.VITE_API_URL_GAME_ATTACK_HINT;
-export const ATTACK_SEND_KEY_URL      = import.meta.env.VITE_API_URL_GAME_ATTACK_SEND_KEY;
-export const DEFENCE_CODE_URL         = import.meta.env.VITE_API_URL_GAME_DEFENCE_CODE;
-export const BATTLE_REVISION_URL      = import.meta.env.VITE_API_URL_GAME_BATTLE_REVISION;
-export const BATTLE_SEND_KEY_URL      = import.meta.env.VITE_API_URL_GAME_BATTLE_SEND_KEY;
+export const START_GAME_URL           = '/game'
+export const FETCH_GAME_START_TIME_URL= '/game/start-time';
+export const GAME_OPPONENT_NAME_URL   = '/game/opponent-name';
+export const GAME_SCORES_URL          = '/game/scores';
+export const ATTACK_CHALLENGE_URL     = '/game/attack/challenge';
+export const ATTACK_HINT_URL          = '/game/attack/hint';
+export const ATTACK_SEND_KEY_URL      = '/game/attack/key';
+export const DEFENCE_CODE_URL         = '/game/defence/code';
+export const BATTLE_REVISION_URL      = '/game/battle/revision';
+export const BATTLE_SEND_KEY_URL      = '/game/battle/key';
