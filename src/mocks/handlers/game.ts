e371aaac5b8b42eb1b401f285e173c9fccb0720b
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-17 02:58:56 +0900

    add : 脆弱性のテストデータ追加

--- src/mocks/handlers/game.ts
+++ src/mocks/handlers/game.ts
@@ -113,7 +113,12 @@ export const attackPhaseHandler= [
               [ CHOICES_KEY ]   : [ "'", "or", "1", "=", "1" ],
               [ HINT_KEY ]      : "4行目の",
               [ HINT_SCORE_KEY ]: -10
-            }
+            },
+            {
+              [ CHOICES_KEY ]   : [ "a", "b", "c", "c", "b" ],
+              [ HINT_KEY ]      : "5行目の",
+              [ HINT_SCORE_KEY ]: -20
+            },
           ]
         }),
         ctx.delay(1000)
@@ -148,7 +153,7 @@ export const attackPhaseHandler= [
         ctx.json(
           {
             [ IS_VALID_KEY ]  : true,
-            [ IS_CORRECT_KEY ]   : true,
+            [ IS_CORRECT_KEY ]: true,
             [ GAME_SCORE_KEY ]: 20,
           }
         ),
@@ -257,7 +262,54 @@ export const battlePhaseHandler= [
         ctx.json(
           {
             [ CODE_PATH_KEY ]: '1/1',
-            [ CODE_KEY ]     : { mockChallenge }
+            [ CODE_KEY ]     : `<html>
+            <head>
+              <style>
+                /* CSSでページの見た目を整える */
+                body {
+                  font-family: Arial, sans-serif;
+                  background-color: lightblue;
+                }
+            
+                .container {
+                  width: 500px;
+                  margin: 0 auto;
+                  padding: 20px;
+                  border: 1px solid black;
+                  background-color: white;
+                }
+            
+                h1 {
+                  text-align: center;
+                }
+            
+                form {
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                }
+            
+                input {
+                  margin: 10px;
+                }
+            
+                button {
+                  width: 100px;
+                  height: 40px;
+                }
+              </style>
+            </head>
+            <body>
+              <div class="container">
+                <h1>ログインページ</h1>
+                <form action="login.php" method="post">
+                  <input type="text" name="user" placeholder="ユーザー名" required>
+                  <input type="password" name="pass" placeholder="パスワード" required>
+                  <button type="submit">ログイン</button>
+                </form>
+              </div>
+            </body>
+            </html>`
           }
         ),
         ctx.delay(1000)
@@ -280,7 +332,7 @@ export const battlePhaseHandler= [
         ctx.json(
           {
             [ IS_VALID_KEY ]  : true,
-            [ IS_CORRECT_KEY ]   : true,
+            [ IS_CORRECT_KEY ]: true,
             [ GAME_SCORE_KEY ]: 20
           }
         ),
