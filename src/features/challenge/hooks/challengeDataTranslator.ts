9d39d6e05b9991a7769d6808561b5a949a86dbf3
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-17 03:04:18 +0900

    add : フェッチしたデータを加工するカスタムフック作成

---
+++ src/features/challenge/hooks/challengeDataTranslator.ts
@@ -0,0 +1,30 @@
+import { useChallengeQuery } from "..";
+import { 
+  CHOICES_KEY, 
+  CODE_PATH_KEY, 
+  HINT_KEY, 
+  HINT_SCORE_KEY, 
+  VULNERABILITIES_KEY 
+} from "@/config/responseKeys";
+
+export const useChallengeDataTranslator= () => {
+  const { data: challengeData } = useChallengeQuery({});
+
+  const challengePath= challengeData?.[ CODE_PATH_KEY ];
+
+  const vulnerabilities = challengeData?.[VULNERABILITIES_KEY].flatMap(
+    (item: { choices: string[]; hint: string; hintScore: number }) => item[CHOICES_KEY]
+  );
+
+  const hintObject= challengeData?.[ VULNERABILITIES_KEY ].map((item, index) => {
+    const hint= item[ HINT_KEY ];
+    const hintScore= item[ HINT_SCORE_KEY ];
+    return {
+      index: index,
+      hint: hint,
+      hintScore: hintScore
+    };
+  });
+  
+  return { challengePath, vulnerabilities, hintObject }
+};
