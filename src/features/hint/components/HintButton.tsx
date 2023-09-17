56957afb2e95f1953c0acf89599d770f17dfca9f
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-17 12:43:01 +0900

    feat : ヒント関連のコンポーネントを作成

---
+++ src/features/hint/components/HintButton.tsx
@@ -0,0 +1,26 @@
+import { useRecoilState } from 'recoil';
+
+import { IconButton } from '@/components/Elements';
+import hintIcon from '@/assets/images/hint.svg';
+import { isHintDrawerState } from '@/atoms';
+
+const _HintButton= `
+  height: 5rem;
+  width : 5rem;
+  position: absolute;
+  bottom: 10px;
+  right : 10px;
+  border-radius: 50%;
+`;
+
+export const HintButton= () => {
+  const [ isDrawHint, setIsDrawHint ]= useRecoilState( isHintDrawerState );
+
+  return (
+    <IconButton
+      icon   = { hintIcon }
+      onClick= { () => setIsDrawHint( !isDrawHint ) }
+      styles = { _HintButton }
+    />
+  );
+};
