3b42976c3bee1c0458fb65b9800ee25028fde97e
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-17 12:40:15 +0900

    add : サインアウト時の処理をカスタムフック化

--- src/features/auth/api/signOut.ts
+++ src/features/auth/api/signOut.ts
@@ -1,6 +1,15 @@
 import { axios } from '@/lib/axios';
-import { SIGNOUT_USER_URL } from '@/config/apiEndpoints';
+import { SIGNOUT_USER_URL } from '@/config/apiUrls';
+import { useSetRecoilState } from 'recoil';
+import { isAuthState } from '@/atoms';
 
-export const signOut= () => {
-  return axios.delete( SIGNOUT_USER_URL );
+export const useSignOut= () => {
+  const setSignOut= useSetRecoilState( isAuthState );
+
+  async function isSignOut() {
+    const signOut= await axios.delete( SIGNOUT_USER_URL );
+    setSignOut( false );
+  };
+
+  return { isSignOut };
 };
