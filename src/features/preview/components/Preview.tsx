f936e1e2ff5a27c35052e256f9a4e30a189c5bfc
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-17 13:32:29 +0900

    add : test用コードを挿入

--- src/features/preview/components/Preview.tsx
+++ src/features/preview/components/Preview.tsx
@@ -2,6 +2,7 @@ import { codeState } from '@/atoms';
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import { createAbsolutePath } from '../selector/createAbsolutePath';
+import { useRef } from 'react';
 
 const _PreviewBox= styled.div<{ styles?: string }>`
   width      : 100%;
@@ -19,22 +20,74 @@ const _Preview= styled.iframe`
 `;
 
 type PreviewProps= {
-  phaseTitle: string;
-  codePath ?: string;
+  iframeRef : React.LegacyRef<HTMLIFrameElement>; 
+  codePath ?: string | number ;
   styles   ?: string;
 };
 
 export const Preview= (
   { 
-    phaseTitle,
+    iframeRef,
+    codePath,
     styles,  
   }: PreviewProps
 ) => {
-  const getCodePath= useRecoilValue( createAbsolutePath(phaseTitle) );
+  // const getCodePath= useRecoilValue( createAbsolutePath(codePath) );
 
   return (
     <_PreviewBox styles={ styles } >
-      <_Preview src= { getCodePath } /> 
+      <_Preview
+        ref    = { iframeRef } 
+        srcDoc= { `
+        <html>
+        <head>
+          <style>
+            /* CSSでページの見た目を整える */
+            body {
+              font-family: Arial, sans-serif;
+              background-color: lightblue;
+            }
+        
+            .container {
+              width: 500px;
+              margin: 0 auto;
+              padding: 20px;
+              border: 1px solid black;
+              background-color: white;
+            }
+        
+            h1 {
+              text-align: center;
+            }
+        
+            form {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+            }
+        
+            input {
+              margin: 10px;
+            }
+        
+            button {
+              width: 100px;
+              height: 40px;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>ログインページ</h1>
+            <form action="login.php" method="post">
+              <input type="text" name="user" placeholder="ユーザー名" required>
+              <input type="password" name="pass" placeholder="パスワード" required>
+              <button type="submit">ログイン</button>
+            </form>
+          </div>
+        </body>
+        </html>` } 
+      /> 
     </_PreviewBox>
   );
 };
