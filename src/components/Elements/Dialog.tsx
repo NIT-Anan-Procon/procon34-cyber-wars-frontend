fef698c4b2d780167bb0fb3d8bee6519f1a32220
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-17 21:52:45 +0900

    add : dialogを表示するコンポーネントを追加

--- src/components/Elements/Dialog.tsx
+++ src/components/Elements/Dialog.tsx
@@ -1,48 +1,48 @@
-// import { useEffect, useRef } from 'react';
-// import styled from 'styled-components';
+import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
 
-// const DialogLayout= styled.dialog`
+const DialogLayout= styled.dialog`
 
-// `;
+`;
 
-// const DialogContent= styled.div`
+const DialogContent= styled.div`
 
-// `;
+`;
 
-// type DialogProps= {
-//   isOpen: boolean
-//   onClose: () => void;
-//   children: React.ReactNode;
-// }
+type DialogProps= {
+  isOpen: boolean
+  onClose: () => void;
+  children: React.ReactNode;
+}
 
-// export const Dialog= ({
-//   isOpen, onClose, children
-// }: DialogProps): React.ReactElement => {
-//   const dialogRef= useRef<HTMLDialogElement>(null);
+export const Dialog= ({
+  isOpen, onClose, children
+}: DialogProps): React.ReactElement => {
+  const dialogRef= useRef<HTMLDialogElement>(null);
   
-//   useEffect((): void => {
-//     const dialogElement= dialogRef.current;
-//     if(!dialogElement) {
-//       return;
-//     }
-//     if(isOpen) {
-//       if(dialogElement.hasAttribute('open')) {
-//         return;
-//       }
-//       dialogElement.showModal();
-//     } else {
-//       if(!dialogElement.hasAttribute('open')) {
-//         return;
-//       }
-//       dialogElement.close();
-//     }
-//   },[isOpen]);
+  useEffect((): void => {
+    const dialogElement= dialogRef.current;
+    if(!dialogElement) {
+      return;
+    }
+    if(isOpen) {
+      if(dialogElement.hasAttribute('open')) {
+        return;
+      }
+      dialogElement.showModal();
+    } else {
+      if(!dialogElement.hasAttribute('open')) {
+        return;
+      }
+      dialogElement.close();
+    }
+  },[isOpen]);
 
-//   return (
-//     <DialogLayout ref={dialogRef} >
-//       <DialogContent>
-//         {children}
-//       </DialogContent>
-//     </DialogLayout>
-//   );
-// }
+  return (
+    <DialogLayout ref={dialogRef} >
+      <DialogContent>
+        {children}
+      </DialogContent>
+    </DialogLayout>
+  );
+}
