27ca978e573ea823d1e20e1bb298c3025bf8221f
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-22 07:42:31 +0900

    add : ユーザ認証関連のエンドポイントを設定

---
+++ src/features/auth/api/config/endpoints.ts
@@ -0,0 +1,7 @@
+export const REGISTER_URL    = '/user';
+export const LOGIN_URL       = '/user/credential';
+export const LOGOUT_URL      = '/user/credential';
+export const IS_LOGGED_IN_URL= '/user/credential';
+
+export const UPDATE_NAME_URL= '/user/name';
+export const UPDATE_USER_PASSWORD_URL= '/user/password';
