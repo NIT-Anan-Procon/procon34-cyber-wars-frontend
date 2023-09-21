f51bf71cea120cbf6ff177fb54ddece7c789413c
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-22 06:56:48 +0900

    remove : スコープを絞るために、削除

--- src/atoms/game/previewCodePathState.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const previewCodePathState= atom({
-  key: 'atom_previewCodePathState',
-  default: undefined
-});
--- src/atoms/game/scoreState.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const scoreState= atom<number>({
-  key    : 'atom_scoreState',
-  default: undefined
-});
