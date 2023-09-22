77a53a71aa69af5f830d961a1f7961169d4a9953
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-22 10:25:58 +0900

    add : ユーザの認証状態を取得する非同期処理を追加

--- src/features/auth/api/fetchAuthenticatedUser.ts
+++ src/features/auth/api/fetchAuthenticatedUser.ts
@@ -1,34 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
-
-import { IS_SIGNEDIN_URL } from '@/config/apiUrls';
 import { axios } from '@/lib/axios';
-import { QueryConfig } from '@/lib/react-query';
-import { useSetRecoilState } from 'recoil';
-import { authenticatedUserState } from '@/atoms';
+import { IS_LOGGED_IN_URL } from './config/endpoints';
 
-export const fetchAuthenticatedUser= () => {
-  const setAuthUser= useSetRecoilState( authenticatedUserState );
-
-  async function authUser() {
-    return await axios.get( IS_SIGNEDIN_URL )
-    .then(( res ) => {
-      setAuthUser( res );
-    })
-  };
-
-  return { authUser };
+export const fetchAuthenticatedUser= async() => {
+  return await axios.get( IS_LOGGED_IN_URL );
 };
-
-// type QueryFn= typeof fetchAuthenticatedUser;
-
-// type UseAuthUserQueryOptions= {
-//   config?: QueryConfig<QueryFn>;
-// };
-
-// export const useAuthUserQuery= ({ config }) => {
-//   return useQuery({
-//     queryKey: [ 'query_authenticatedUser' ],
-//     queryFn : () => fetchAuthenticatedUser(),
-//     ...config
-//   });
-// };
