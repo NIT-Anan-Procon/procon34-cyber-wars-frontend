cd4f88f3cf38a7e3e9b9f3dd86c5338d98090051
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-22 22:45:45 +0900

    remove

--- src/features/room/api/createRoom/hook.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-
-import { createRoomFn } from './mutationFn';
-
-export const useCreateRoomMutation= () => {
-  return useMutation({
-    mutationFn: createRoomFn
-  })
-};
