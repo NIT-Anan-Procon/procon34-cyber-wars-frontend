6afd7e7dcca575869d11667d6b888d6ce3110e4a
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-23 12:41:44 +0900

    add : レスポンスキーを追加

---
+++ src/features/challenge/api/constants/responseKeys.ts
@@ -0,0 +1,5 @@
+export const CODE_PATH_KEY       = "path";
+export const VULNERABILITIES_KEY = "vulnerabilities";
+export const CHOICES_KEY         = "choices";
+export const HINT_KEY            = "hint";
+export const HINT_SCORE_KEY      = "hintScore";
