799015f73b37a55cf0f4e8ef454b63193efc7b15
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-24 00:23:20 +0900

    add : 認証が成功、失敗した際の処理を追加

--- src/features/auth/api/login/useLoginMutation.ts
+++ src/features/auth/api/login/useLoginMutation.ts
@@ -1,7 +1,8 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { MutationConfig } from '@/lib/react-query';
 import { loginFn } from './loginFn';
+import { queryClient, MutationConfig } from '@/lib/react-query';
+import { AuthResponseType, AuthenticatedUserQueryKey, IS_LOGGED_IN_KEY } from '..';
 
 type UseLoginMutationOptions= {
   config?: MutationConfig<typeof loginFn>
@@ -9,6 +10,20 @@ type UseLoginMutationOptions= {
 
 export const useLoginMutation= ({ config }: UseLoginMutationOptions) => {
   return useMutation({
+    onSuccess: ( isSuccess: AuthResponseType ) => {
+      const prevIsAuthenticated= queryClient.getQueryData( AuthenticatedUserQueryKey );
+      
+      if( prevIsAuthenticated && isSuccess ) {
+        queryClient.setQueryData( AuthenticatedUserQueryKey, {
+          ...prevIsAuthenticated,
+          [ IS_LOGGED_IN_KEY ]: isSuccess,
+        });
+      }
+    },
+    onError: ( isSuccess: AuthResponseType ) => {
+      alert('ログインに失敗しました')
+    },
+    ...config,
     mutationFn: loginFn
   });
 };
--- src/features/auth/api/register/useRegisterMutation.ts
+++ src/features/auth/api/register/useRegisterMutation.ts
@@ -1,9 +1,29 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { registerFn } from './registerFn';
+import { MutationConfig, queryClient } from '@/lib/react-query';
+import { AuthResponseType, AuthenticatedUserQueryKey, IS_LOGGED_IN_KEY } from '..';
 
-export const useRegisterMutation= () => {
+type UseRegisterMutationOptions= {
+  config?: MutationConfig<typeof registerFn>;
+};
+
+export const useRegisterMutation= ({ config }: UseRegisterMutationOptions ) => {
   return useMutation({
+    onSuccess: ( isSuccess: AuthResponseType ) => {
+      const prevIsAuthenticated= queryClient.getQueryData( AuthenticatedUserQueryKey );
+      
+      if( prevIsAuthenticated && isSuccess ) {
+        queryClient.setQueryData( AuthenticatedUserQueryKey, {
+          ...prevIsAuthenticated,
+          [ IS_LOGGED_IN_KEY ]: isSuccess,
+        });
+      }
+    },
+    onError: ( isSuccess: AuthResponseType ) => {
+      alert('ユーザ登録に失敗しました')
+    },
+    ...config,
     mutationFn: registerFn
   });
 };
