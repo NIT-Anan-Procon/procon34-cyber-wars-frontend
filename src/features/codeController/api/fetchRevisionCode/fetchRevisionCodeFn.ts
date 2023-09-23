6a7894597063c316ff4830c926417bdda7668855
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-23 13:31:49 +0900

    refactor : インポート修正

--- src/features/codeController/api/fetchRevisionCode/fetchRevisionCodeFn.ts
+++ src/features/codeController/api/fetchRevisionCode/fetchRevisionCodeFn.ts
@@ -1,8 +1,8 @@
 import { AxiosResponse } from 'axios';
 
 import { axios } from '@/lib/axios';
-import { BATTLE_REVISION_URL, CodeResponseType } from '..';
+import { BATTLE_REVISION_URL, RevisionCodeResponseType } from '..';
 
-export const fetchRevisionCodeFn= async(): Promise<AxiosResponse<CodeResponseType>> => {
+export const fetchRevisionCodeFn= async(): Promise<AxiosResponse<RevisionCodeResponseType>> => {
   return await axios.get( BATTLE_REVISION_URL );
 };
