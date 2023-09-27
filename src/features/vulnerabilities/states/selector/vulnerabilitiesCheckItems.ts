8ec2736367dbafbc2965f8321810c0f4d4ae3cd3
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-27 21:27:41 +0900

    comment out

--- src/features/vulnerabilities/states/selector/vulnerabilitiesCheckItems.ts
+++ src/features/vulnerabilities/states/selector/vulnerabilitiesCheckItems.ts
@@ -1,6 +1,24 @@
-import { selector } from 'recoil';
+// import { selectorFamily } from 'recoil';
+// import { checkedVulnerabilies } from '../atom';
 
-export const vulnerabilitiesCheckItems= selector({
-  key: '',
-  default:
-});
+// export const vulnerabilityCheckItems= selectorFamily({
+//   key: 'selector_vulnerabilityCheckItems',
+//   get: ( data ) => ({ get }) => {
+//     const vulnerabilityListState= get( checkedVulnerabilies );
+
+//     const vulnerabilityCheckItems= data?.map(
+//       ( value: string, index: number ) => {
+//         return {
+//           id: index,
+//           value: value,
+//           checked: false
+//         }
+//       }
+//     );
+
+//     return vulnerabilityCheckItems;
+//   },
+//   set: ( data ) => ({ set }, newState ) => {
+//     set( checkedVulnerabilies, newState );
+//   }
+// });
