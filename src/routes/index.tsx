00c0e231c8b8a00cf20602a8e68d87335092db94
kusaka0324 <haruto.k.4230@gmail.com>
2023-09-29 16:13:15 +0900

    refactor : 不要なコメントアウト削除

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,21 +5,23 @@ import { Landing } from '@/features/misc';
 import { authorizedRoutes } from './authorizedRoutes';
 import { publicRoutes }     from './publicRoutes';
 import { AuthenticatedResponseType, IS_LOGGED_IN_KEY, useAuthenticatedUserQuery } from '@/features/auth';
+import { ModeSelection } from '@/features/modeSelect';
 
 
 export const AppRoutes = () => {
-  // const { data: isAuthenticated }= useAuthenticatedUserQuery({
-  //   config: {
-  //     select: ( authUser:AuthenticatedResponseType ) => authUser[ IS_LOGGED_IN_KEY ]
-  //   }
-  // });
-  const isAuthenticated= true;
+  const { data: isAuthenticated, isLoading }= useAuthenticatedUserQuery({
+    config: {
+      select: ( authUser:AuthenticatedResponseType ) => authUser[ IS_LOGGED_IN_KEY ]
+    }
+  });
 
-  const commonRoutes= [{ path: '/', element: <Landing /> }];
-  
+  if( isLoading ) {
+    return <>loading</>
+  };
+
   const routes = isAuthenticated ? authorizedRoutes : publicRoutes;
 
-  const element = useRoutes([...routes, ...commonRoutes]);
+  const element = useRoutes([...routes]);
 
   return <>{ element }</>;
 }
