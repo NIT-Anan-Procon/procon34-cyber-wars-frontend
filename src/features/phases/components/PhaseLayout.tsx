5daf8200e7eb2615d0da4af2d9101710ce37decc
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-02 00:04:54 +0900

    refactor : インポート修正

--- src/features/phases/components/PhaseLayout.tsx
+++ src/features/phases/components/PhaseLayout.tsx
@@ -1,12 +1,12 @@
-import { Head } from '@/components/Head';
 import styled from 'styled-components';
+
+import { Head } from '@/components/Head';
 
 const _PhaseLayout= styled.div`
   height : 100vh;
   width  : 100vw;
   display: flex;
   flex-direction: column;
-  row-gap: 20px;
 `;
 
 type PhaseLayoutProps= {
