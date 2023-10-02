b16b99213f5a9412bdf66ff91f66becdeeb5e710
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-03 03:13:31 +0900

    fix : 初期時間の変更

--- src/features/gameRules/states/atom/settingTimeState.ts
+++ src/features/gameRules/states/atom/settingTimeState.ts
@@ -1,6 +1,11 @@
-import { atomFamily } from 'recoil';
+import { PHASE } from '@/features/phases';
+import { atom } from 'recoil';
 
-export const settingTimeState= atomFamily({
-  key: 'atomFamily_settingTime',
-  default: ( phase: string ) => ( undefined )
+export const settingTimeState= atom({
+  key: 'atom_settingTime',
+  default: {
+    [ PHASE.ATTACK_PHASE ] : 5,
+    [ PHASE.DEFENCE_PHASE ]: 5,
+    [ PHASE.BATTLE_PHASE ] : 5
+  }
 });
