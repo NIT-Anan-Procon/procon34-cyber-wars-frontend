90ab6aee84adf0687bc86d694e0d0a41dc3093b4
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-09 05:02:33 +0900

    style : レイアウト修正

--- src/features/games/choices/components/ChoicesInputFieldWrapper.tsx
+++ src/features/games/choices/components/ChoicesInputFieldWrapper.tsx
@@ -4,36 +4,51 @@ import styled from 'styled-components';
 import { InputField } from '@/components/Form';
 import { targetInputNameState } from '@/features/games/webViewer';
 import { colors } from '@/assets/styles';
+import { checkedChoiceValuesSelector } from '../states/selector';
+import { ChoicesEnterButton } from '.';
 
 const _ChoicesInputFieldWrapper= styled.div`
   height: 100%;
-  width : 75%;
+  width : 100%;
   padding-left: 12px;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  justify-content: end;
+  row-gap: 10px;
   border-radius: 5px;
 `;
 
-const _TargetName= styled.div`
-  font-size: 1.6rem;
-  color : ${ colors.primary };
+const _TargetName= styled.div<{ isTarget: string }>`
+  font-size: 2rem;
+  color: 
+    ${(props) => !props.isTarget
+      ? `${ colors.danger }`
+      : `${ colors.primary }`
+    };
 `;
 
 const _InputArea= styled.div`
+  display: flex;
+  column-gap: 20px;
 `;
 
 export const ChoicesInputFieldWrapper= () => {
   const targetInputName= useRecoilValue( targetInputNameState );
+  const checkedChoiceValues= useRecoilValue( checkedChoiceValuesSelector );
 
   return (
     <_ChoicesInputFieldWrapper>
-      <_TargetName>
+      <_TargetName isTarget={ targetInputName } >
         {
           ! targetInputName
-          ? 'ターゲットを指定してください'
+          ? <span>ターゲットを指定してください</span>
           : <span>Target: { targetInputName }</span>
-        }</_TargetName>
-      <_InputArea ><InputField id={'choices'} /></_InputArea>
+        }
+      </_TargetName>
+      <_InputArea >
+        <InputField id={'choices'} value={ checkedChoiceValues } />
+        <ChoicesEnterButton />
+      </_InputArea>
     </_ChoicesInputFieldWrapper>
   );
 };
