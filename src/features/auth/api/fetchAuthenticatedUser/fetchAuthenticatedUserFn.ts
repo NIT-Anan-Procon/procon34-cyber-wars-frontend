c27a2d77d1f1fb60a550962000af7cd9df97ab59
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-10 15:38:02 +0900

    add : 非同期処理のレスポンスの型修正

--- src/features/auth/api/fetchAuthenticatedUser/fetchAuthenticatedUserFn.ts
+++ src/features/auth/api/fetchAuthenticatedUser/fetchAuthenticatedUserFn.ts
@@ -1,9 +1,7 @@
-import { AxiosResponse } from 'axios';
-
 import { axios } from '@/lib/axios';
 import { IS_LOGGED_IN_URL } from '../constants';
 import { AuthenticatedResponseType } from '../types';
 
-export const fetchAuthenticatedUserFn= async(): Promise<AxiosResponse<AuthenticatedResponseType>> => {
+export const fetchAuthenticatedUserFn= async(): Promise<AuthenticatedResponseType> => {
   return await axios.get( IS_LOGGED_IN_URL );
 };
