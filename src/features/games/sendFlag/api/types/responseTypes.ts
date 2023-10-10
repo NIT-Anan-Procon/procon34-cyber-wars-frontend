14d41f7e1e8dd4e85690ee06453677471d0779a3
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-11 00:43:43 +0900

    type : 型修正

--- src/features/games/sendFlag/api/types/responseTypes.ts
+++ src/features/games/sendFlag/api/types/responseTypes.ts
@@ -1,11 +1,5 @@
-import { 
-  IS_CORRECT_KEY,
-  IS_VALID_KEY,
-  GAME_SCORE_KEY
-} from '../constants';
-
 export type SendFlagResponseType= {
-  [ IS_VALID_KEY ]  : boolean;
-  [ IS_CORRECT_KEY ]: boolean;
-  [ GAME_SCORE_KEY ]: number;
+  valid: boolean;
+  correct: boolean;
+  score: number;
 };
