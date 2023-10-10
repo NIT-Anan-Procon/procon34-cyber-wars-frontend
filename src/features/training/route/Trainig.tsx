e72a216eabbac7f9188d474c903f4bbfcefe7ef2
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-10 15:31:14 +0900

    add : 各説明のスライドを追加

--- src/features/training/route/Trainig.tsx
+++ src/features/training/route/Trainig.tsx
@@ -1,34 +1,31 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper/modules'
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-// カルーセルにする画像のソースをリストにします
-const images = ['/dummy/1.jpg', '/dummy/2.jpg', '/dummy/3.jpg']
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+import { AttackPhaseSlide, BattlePhaseSlide, DefencePhaseSlide, EndSlide, ExplanationSlide, InitialSlide } from '../slide';
 
 export const Training= () => {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Autoplay]}
-      slidesPerView={1} //一度に表示するスライドの数
-      pagination={{
-        clickable: true,
-      }} //何枚目のスライドかを示すアイコン、スライドの下の方にある
-      navigation //スライドを前後させるためのボタン、スライドの左右にある
-      loop={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-    >
-      
-        return (
-          <SwiperSlide >
-            <h1>1</h1>
-          </SwiperSlide>
-        )
-
-    </Swiper>
-  )
+    <>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide><InitialSlide /></SwiperSlide>
+        <SwiperSlide><AttackPhaseSlide /></SwiperSlide>
+        <SwiperSlide><DefencePhaseSlide /></SwiperSlide>
+        <SwiperSlide><BattlePhaseSlide /></SwiperSlide>
+        <SwiperSlide><ExplanationSlide /></SwiperSlide>
+        <SwiperSlide><EndSlide /></SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
