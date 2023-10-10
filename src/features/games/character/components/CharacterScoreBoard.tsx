b0d98e6cac2cf73ad4416dc515371c1426cb2ee1
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-10 18:15:01 +0900

    refactor : 不要なスタイル削除

--- src/features/games/character/components/CharacterScoreBoard.tsx
+++ src/features/games/character/components/CharacterScoreBoard.tsx
@@ -4,6 +4,7 @@ import { SCORES_KEY, useFetchScoresQuery } from '@/features/games/scores';
 import { colors } from '@/assets/styles';
 import { Loading } from '@/components/Animation';
 import { IS_HOST_KEY, useFetchRoomInfoQuery } from '../../room';
+import { ScoreHistory } from '../../scores/components/ScoreHistory';
 const _CharacterScoreBoardWrapper= styled.div<CharacterScoreBoardTransTypes>`
   height     : 100%;
   width      : 100vw;
@@ -69,57 +70,6 @@ const _CharacterName= styled.h1<CharacterScoreBoardTransTypes>`
           color: ${ colors.redAccent };
           transform: skew( -3deg );
       `
-  };
-`;
-
-const _AddScore= styled.div<CharacterScoreBoardTransTypes>`
-  height: 8rem;
-  width : 15rem;
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  
-  &::before,
-  &::after {
-    content: '';
-    position: absolute;
-    height: 100%;
-    width : 100%;
-  };
-  ${(props) => props.status === 'HOST'
-    ? css`
-        right: 10%;
-        &::before {
-          background: black;
-          clip-path: polygon(10% 15%, 96% 4%, 90% 88%, 44% 78%, 28% 96%, 36% 73%, 4% 71%);
-          z-index: 200;
-        };
-        &::after {
-          background: ${ colors.bgLighter };
-          clip-path: polygon(6% 11%, 100% 0%, 94% 96%, 48% 82%, 24% 100%, 32% 77%, 0 75%);
-          z-index: 199;
-        };
-      `
-    : css`
-        left: 10%;
-        &::before {
-          background: black;
-          clip-path: polygon(4% 4%, 90% 15%, 96% 71%, 64% 73%, 72% 96%, 44% 78%, 10% 88%);
-          z-index: 200;
-        };
-        &::after {
-          background: ${ colors.bgLighter };
-          clip-path: polygon(0% 0%, 94% 11%, 100% 75%, 68% 77%, 76% 100%, 48% 82%, 6% 92%);
-          z-index: 199;
-        };
-      `
-  };
-
-  > span {
-    font-size: 2.75rem;
-    color: ${ colors.primary };
-    z-index: 1000;
   };
 `;
 
@@ -208,10 +158,10 @@ export const CharacterScoreBoard= (
       <_CharacterNameWrapper status={ status } >
         <_CharacterName status={ status } >{ userName }</_CharacterName>
       </_CharacterNameWrapper>
-      {/* { myUserStatus === status
+      { myUserStatus === status
         ? <ScoreHistory />
         : undefined
-      } */}
+      }
       <_ScoreWrapper status={ status } >
         <_Score>
           { status === myUserStatus
