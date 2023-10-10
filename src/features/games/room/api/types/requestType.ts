70ba02200c6842d579e9536e9d6af13860098164
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-10 16:31:16 +0900

    type : 不要な型宣言削除

--- src/features/games/room/api/types/requestType.ts
+++ src/features/games/room/api/types/requestType.ts
@@ -1,8 +1,4 @@
-import { INVITE_ID_KEY, IS_DIFFICULT_KEY } from '..';
-
-export type CreateRoomRequestType= {
-  [ IS_DIFFICULT_KEY ]: boolean;
-};
+import { INVITE_ID_KEY } from '..';
 
 export type JoinRoomRequestType= {
   [ INVITE_ID_KEY ]: number;
