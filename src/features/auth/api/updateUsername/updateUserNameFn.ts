2b60a24783efe5e2f30c82b5bbbc0fb850fc2648
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-10 15:37:24 +0900

    add : 非同期処理のレスポンスの型修正

--- src/features/auth/api/updateUsername/updateUserNameFn.ts
+++ src/features/auth/api/updateUsername/updateUserNameFn.ts
@@ -1,10 +1,8 @@
-import { AxiosResponse } from 'axios';
-
 import { axios } from '@/lib/axios';
 import { UPDATE_USERNAME_URL, USER_NAME_KEY } from '../constants';
 import { AuthResponseType } from '../types';
 
-export const updateUsernameFn= async( name: string ): Promise<AxiosResponse<AuthResponseType>> => {
+export const updateUsernameFn= async( name: string ): Promise<AuthResponseType> => {
   const userNameJson= JSON.stringify({ [ USER_NAME_KEY ]: name });
 
   return await axios.patch( UPDATE_USERNAME_URL, userNameJson );
