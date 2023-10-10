e82e7718904c825b9b0a150ef7cc29e593902b86
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-10 16:28:23 +0900

    type : レスポンスの型修正

--- src/features/games/challenge/api/fetchChallenge/fetchChallengeFn.ts
+++ src/features/games/challenge/api/fetchChallenge/fetchChallengeFn.ts
@@ -1,10 +1,8 @@
-import { AxiosResponse } from 'axios';
-
 import { axios } from '@/lib/axios';
 import { CHALLENGE_URL } from '../constants';
 import { ChallengeResponseType } from '../types';
 
 
-export const fetchChallengeFn= async(): Promise<AxiosResponse<ChallengeResponseType>> => {
+export const fetchChallengeFn= async(): Promise<ChallengeResponseType> => {
   return await axios.get( CHALLENGE_URL );
 };
