ff5f98bab44523c90cd2af9d995a54f837dc9e5e
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-10 10:01:11 +0900

    style : レイアウト修正

--- src/features/games/hint/components/HintContentWrapper.tsx
+++ src/features/games/hint/components/HintContentWrapper.tsx
@@ -40,8 +40,10 @@ const _HintContainer= styled.div`
   height : 50%;
   width  : 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  row-gap: 20px;
 `;
 
 type HintContentWrapperProps= {
