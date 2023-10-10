e4d0bceded4387126e78e21da2e1624f11c99237
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-10 15:30:46 +0900

    add : initial commit

--- src/features/training/slide/components/SlideWrapper.tsx
+++ src/features/training/slide/components/SlideWrapper.tsx
@@ -1,5 +1,25 @@
-export const SlideWrapper= () => {
+import { Header } from '@/components/Elements';
+import styled from 'styled-components';
+
+const _SlideWrapper= styled.div`
+  height: 100vh;
+  width : 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+`;
+
+type SlideWrapperProps= {
+  title  ?: string;
+  children: React.ReactNode;
+};
+
+export const SlideWrapper= ({ title, children }: SlideWrapperProps ) => {
   return (
-    <></>
+    <_SlideWrapper>
+      <Header title={ title } />
+      { children }
+    </_SlideWrapper>
   );
 };
