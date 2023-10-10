ed6b43b874bc04aa0f4569febb6f53ddd64bb2fd
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-10 12:13:35 +0900

    add : initial commit

--- src/features/training/route/Trainig.tsx
+++ src/features/training/route/Trainig.tsx
@@ -1,5 +1,34 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+// カルーセルにする画像のソースをリストにします
+const images = ['/dummy/1.jpg', '/dummy/2.jpg', '/dummy/3.jpg']
+
 export const Training= () => {
   return (
-    <></>
-  );
-};
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      slidesPerView={1} //一度に表示するスライドの数
+      pagination={{
+        clickable: true,
+      }} //何枚目のスライドかを示すアイコン、スライドの下の方にある
+      navigation //スライドを前後させるためのボタン、スライドの左右にある
+      loop={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+    >
+      
+        return (
+          <SwiperSlide >
+            <h1>1</h1>
+          </SwiperSlide>
+        )
+
+    </Swiper>
+  )
+}
