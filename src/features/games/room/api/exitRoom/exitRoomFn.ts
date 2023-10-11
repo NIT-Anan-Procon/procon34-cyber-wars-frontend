0cd7d4a422da2e36c0716cc22c4ba76fe0200374
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-11 12:42:53 +0900

    fix : ルーム退出の非同期処理修正

--- src/features/games/room/api/exitRoom/exitRoomFn.ts
+++ src/features/games/room/api/exitRoom/exitRoomFn.ts
@@ -1,8 +1,6 @@
-import { AxiosResponse } from 'axios';
-
 import { axios } from '@/lib/axios';
 import { ROOM_URL } from '../constants';
 
-export const exitRoomFn= async(): Promise<AxiosResponse<void>> => {
+export const exitRoomFn= async(): Promise<[]> => {
   return await axios.delete( ROOM_URL );
 };
