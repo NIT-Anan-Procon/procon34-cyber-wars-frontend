192570b3fbb1841de59ed442c18c22bf57e07cbd
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-12 04:55:03 +0900

    add : 認証状態を表示するalert追加

--- src/features/auth/api/login/useLoginMutation.ts
+++ src/features/auth/api/login/useLoginMutation.ts
@@ -3,17 +3,26 @@ import { useMutation } from '@tanstack/react-query';
 import { loginFn } from './loginFn';
 import { queryClient } from '@/lib/react-query';
 import { AuthResponseType, AuthenticatedUserQueryKey } from '..';
+import { useNavigate } from 'react-router';
 
 export const useLoginMutation= () => {
+  const navigate= useNavigate();
+
   return useMutation({
-    onSuccess: ( isSuccess: AuthResponseType ) => {
+    onSuccess: ( authUser: AuthResponseType ) => {
       const prevIsAuthenticated= queryClient.getQueryData( AuthenticatedUserQueryKey );
       
-      if( prevIsAuthenticated && isSuccess.success ) {
+      if( prevIsAuthenticated && authUser.success ) {
         queryClient.setQueryData( AuthenticatedUserQueryKey, {
           ...prevIsAuthenticated,
-          loggedIn: isSuccess.success,
+          loggedIn: authUser.success,
         });
+      }
+
+      if( authUser.success ) {
+        navigate( '../../' )
+      } else {
+        alert('ログインに失敗しました。')
       }
     },
     mutationFn: loginFn
