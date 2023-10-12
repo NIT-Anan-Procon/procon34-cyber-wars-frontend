dcf42c3437e71689d2204564cc09ea4287b3067a
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-12 12:27:28 +0900

    fix : refを取得して、カウント値を安定化する

--- src/features/games/gameTimer/components/Timer.tsx
+++ src/features/games/gameTimer/components/Timer.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-
-import { useQuery } from '@tanstack/react-query';
-import { StartTimeQueryKey, fetchStartTimeFn } from '..';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
+import { StartTimeQueryKey, fetchStartTimeFn } from '..';
 
 const _Timer= styled.h1`
   font-size: 4rem;
@@ -23,9 +23,11 @@ export const Timer = ({ targetTime, redirectUrl }: TimerProps) => {
       refetchOnMount: true
     }
   );
+  const countdownRef = useRef(countdown);
+  countdownRef.current = countdown;
 
   useEffect(() => {
-    if(!startTimeQuery?.data) return;  
+    if( !startTimeQuery?.data ) return;
     const gameStartTime: Date = new Date( startTimeQuery?.data?.startTime );
     const milliseconds: number= gameStartTime.getTime();
     const endTime: Date= new Date(new Date(milliseconds + targetTime * 1000));
@@ -36,7 +38,7 @@ export const Timer = ({ targetTime, redirectUrl }: TimerProps) => {
     if ( diffTime > 0 ) {
       setCountdown( Math.floor(diffTime / 1000) );
       const timer = setInterval(() => {
-        if (countdown !== null && countdown !== undefined) {
+        if (countdownRef.current !== null && countdownRef.current !== undefined) {
           setCountdown(( prevCountdown: number | undefined ) => {
             if ( prevCountdown !== undefined && prevCountdown >= 1) {
               return prevCountdown - 1;
@@ -59,6 +61,7 @@ export const Timer = ({ targetTime, redirectUrl }: TimerProps) => {
   }, [ navigate, redirectUrl, startTimeQuery?.data?.startTime ] )
 
   const  SECONDS_IN_MINUTE= 60;
+
   const formatCountdown = (): string | undefined => {
     if (countdown !== undefined ) {  
       let minutes: string | number = Math.floor(countdown / SECONDS_IN_MINUTE);
