991db35ad7efb887c4fd5e4b25c2de134eda79f1
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-13 03:14:36 +0900

    fix : スキーマの設定変更

--- src/features/games/gameRules/types/settingRuleSchema.ts
+++ src/features/games/gameRules/types/settingRuleSchema.ts
@@ -7,6 +7,5 @@ export const settingRuleSchema= z.object({
     required_error: "必須項目です",
     invalid_type_error: "数値を入力してください"
   })
-  .min( 1, '1分以上に設定してください' )
   .max( 20, '20分以下に設定してください' )
 });
