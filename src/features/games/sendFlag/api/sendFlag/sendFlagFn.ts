bfbae1a7ecbb04110c51c3691738a5953926fa52
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-12 15:02:23 +0900

    fix : keyの変更

--- src/features/games/sendFlag/api/sendFlag/sendFlagFn.ts
+++ src/features/games/sendFlag/api/sendFlag/sendFlagFn.ts
@@ -1,9 +1,8 @@
 import { axios } from '@/lib/axios';
 import { SendFlagResponseType } from '../types';
-import { FLAG_KEY } from '../constants';
 
 export const sendFlagFn= async({ endpoint, flag }: { endpoint: string, flag: string }): Promise<SendFlagResponseType> => {
-  const flagJson= JSON.stringify({ [ FLAG_KEY ]: flag });
+  const flagJson= JSON.stringify({ key: flag });
 
   return await axios.post( endpoint, flagJson );
 };
