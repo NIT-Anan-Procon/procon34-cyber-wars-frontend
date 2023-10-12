555fd035c78d127fef75db1fbf79119d80935cf3
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-12 16:24:11 +0900

    add : defaultの制限時間修正

--- src/features/games/gameRules/types/settingRuleSchema.ts
+++ src/features/games/gameRules/types/settingRuleSchema.ts
@@ -7,6 +7,6 @@ export const settingRuleSchema= z.object({
     required_error: "必須項目です",
     invalid_type_error: "数値を入力してください"
   })
-  .min( 3, '3分以上に設定してください' )
+  .min( 1, '1分以上に設定してください' )
   .max( 20, '20分以下に設定してください' )
 });
