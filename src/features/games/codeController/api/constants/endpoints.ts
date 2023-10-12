35de961adab04e7bc580299e848d839356346bcb
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-12 23:46:03 +0900

    add : エンドポイント追加

--- src/features/games/codeController/api/constants/endpoints.ts
+++ src/features/games/codeController/api/constants/endpoints.ts
@@ -1,2 +1,3 @@
-export const DEFENCE_CODE_URL   = `/game/defence/code`;
-export const BATTLE_REVISION_URL= `/game/battle/revision`;
+export const DEFENCE_REVISION_URL= `/game/defence/revision-path`;
+export const DEFENCE_CODE_URL    = `/game/defence/code`;
+export const BATTLE_REVISION_URL = `/game/battle/revision`;
