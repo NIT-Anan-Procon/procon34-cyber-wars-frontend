27c4616ab128df5870c824b80eb6fba9f407f7d7
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-14 02:10:47 +0900

    add : キャッシュクリア追加

--- src/features/games/matching/api/deleteGame/useDeleteGameMutation.ts
+++ src/features/games/matching/api/deleteGame/useDeleteGameMutation.ts
@@ -3,12 +3,14 @@ import { useMutation } from '@tanstack/react-query';
 import { deleteGameFn } from '.';
 import { useSetRecoilState } from 'recoil';
 import { isShowHintState } from '@/features/games/hint';
+import { queryClient } from '@/lib/react-query';
 
 export const useDeleteGameMutation= () => {
   const setIsHint= useSetRecoilState( isShowHintState );
 
   return useMutation({
     onSuccess: () => {
+      queryClient.clear()
       setIsHint( false )
     },
     mutationFn: deleteGameFn
