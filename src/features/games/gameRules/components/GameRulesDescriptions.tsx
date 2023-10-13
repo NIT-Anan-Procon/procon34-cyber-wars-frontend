c969ff7df8516c119995c977ed3d7dbb28c19f1e
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-13 21:47:59 +0900

    add : 更新ボタン追加

--- src/features/games/gameRules/components/GameRulesDescriptions.tsx
+++ src/features/games/gameRules/components/GameRulesDescriptions.tsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import { GameRuleContentWrapper, GameRuleEditForm, GameRuleSettingWrapper } from '.';
 import { RULES } from '../types/ruleDescriptions';
 import { PHASE } from '@/features/games/phases';
+import { useRecoilValue } from 'recoil';
+import { settingTimeState } from '..';
+import { useUpdateTimeLimitMutation } from '../../room/api/updateTimeLimit';
+import { Button } from '@/components/Elements';
 
 const _GameRulesDescriptionsWrapper= styled.div`
   height: 100%;
@@ -18,7 +22,22 @@ const _GameRuleSettings= styled.div`
   row-gap: 10px;
 `;
 
+const $SaveTimeButton= styled(Button)`
+  position: absolute;
+  right   : 40px;
+  bottom  : -10px;
+  height  : 6rem;
+  width   : 12rem;
+  font-size: 1.6rem;
+  clip-path: polygon(2% 6%, 96% 1%, 93% 100%, 5% 96%);
+  z-index: 999;
+`;
+
 export const GameRulesDescriptions= () => {
+  const timeLimit= useRecoilValue( settingTimeState );
+  const updateTimeLimitMutation= useUpdateTimeLimitMutation();
+  
+
   return (
     <_GameRulesDescriptionsWrapper>
       <GameRuleContentWrapper
@@ -39,6 +58,14 @@ export const GameRulesDescriptions= () => {
           <GameRuleEditForm phase={ PHASE.BATTLE_PHASE } />
         </GameRuleSettingWrapper>        
       </_GameRuleSettings>
+      <$SaveTimeButton 
+        type='button'
+        onClick={
+          async() => await updateTimeLimitMutation.mutateAsync( timeLimit )
+        }
+      >
+        Save
+      </$SaveTimeButton>
     </_GameRulesDescriptionsWrapper>
   );
 };
