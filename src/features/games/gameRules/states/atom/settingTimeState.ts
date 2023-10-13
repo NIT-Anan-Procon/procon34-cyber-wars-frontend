a028caa2a33f720ae07a6ef691b710ac51728425
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-13 21:53:35 +0900

    fix : デフォルトの値を修正

--- src/features/games/gameRules/states/atom/settingTimeState.ts
+++ src/features/games/gameRules/states/atom/settingTimeState.ts
@@ -1,11 +1,10 @@
-import { PHASE } from '@/features/games/phases';
 import { atom } from 'recoil';
 
 export const settingTimeState= atom({
   key: 'atom_settingTime',
   default: {
-    [ PHASE.ATTACK_PHASE ] : 1,
-    [ PHASE.DEFENCE_PHASE ]: 1,
-    [ PHASE.BATTLE_PHASE ] : 1
+    attackPhase : 60,
+    defencePhase: 60,
+    battlePhase : 60
   }
 });
