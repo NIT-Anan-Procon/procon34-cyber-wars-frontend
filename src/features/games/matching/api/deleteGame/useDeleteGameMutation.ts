6aab51f389ad7b3f30b95ef70ba677c6999fef1c
kusaka0324 <haruto.k.4230@gmail.com>
2023-10-14 21:06:32 +0900

    fix : インポート修正

--- src/features/games/matching/api/deleteGame/useDeleteGameMutation.ts
+++ src/features/games/matching/api/deleteGame/useDeleteGameMutation.ts
@@ -2,23 +2,24 @@ import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 import { deleteGameFn } from '.';
-import { REMATCH_SUCCESSFUL } from '../types';
 import { queryClient } from '@/lib/react-query';
 import { useSetRecoilState } from 'recoil';
 import { isShowHintState } from '@/features/games/hint';
+import { useExitRoomMutation } from '@/features/games/room';
 
 export const useDeleteGameMutation= () => {
   const navigate= useNavigate();
   const setIsHint= useSetRecoilState( isShowHintState );
+  const exitRoomMutation= useExitRoomMutation();
 
   return useMutation({
-    onSuccess: ( data: REMATCH_SUCCESSFUL ) => {
-      if( data.success ) {
+    onMutate: async( isRematch: boolean ) => {
+      if( isRematch ) {
         queryClient.clear()
         setIsHint( false )
         navigate('../standby');
       } else {
-        alert('課題が存在しません');
+        await exitRoomMutation.mutateAsync()
       };
     },
     mutationFn: deleteGameFn
