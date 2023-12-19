513bb130b823282282f895d72a5c8352d9d558c5
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2023-12-19 13:48:26 +0900

    Update Button.tsx

--- src/components/Elements/Button.tsx
+++ src/components/Elements/Button.tsx
@@ -22,7 +22,7 @@ const _Button= styled.button<{ active?: boolean }>`
   }
 
   &:active {
-    transform: scale(1.1) translateX(-5%) translateY(-5%);
+    transform: scale(1.1);
   }
 
   ${(props) => props.active
